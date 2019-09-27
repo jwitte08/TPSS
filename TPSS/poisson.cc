beb6345d5bf164a9770e5999f378167f243fd5d0
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-09-26 21:39:03 -0400

    fix 'write_table' and add mg timings

--- TPSS/poisson.cc
+++ TPSS/poisson.cc
@@ -21,75 +21,74 @@ struct Timings
 
 struct TestParameter
 {
-  TPSS::PatchVariant    patch_variant = CT::PATCH_VARIANT_; // TPSS::PatchVariant::cell;
-  TPSS::SmootherVariant smoother_variant =
-    CT::SMOOTHER_VARIANT_; // TPSS::SmootherVariant::multiplicative;
-  double   cg_reduction        = 1.e-8;
-  unsigned n_refinements       = 1;
-  unsigned n_cycles            = 1;
-  unsigned n_smoothing_steps   = 1;
-  unsigned n_samples           = 10;
-  unsigned n_subsamples_vmult  = 4;
-  unsigned n_subsamples_smooth = 4;
-  unsigned n_subsamples_mg     = 2;
+  TPSS::PatchVariant    patch_variant       = CT::PATCH_VARIANT_;
+  TPSS::SmootherVariant smoother_variant    = CT::SMOOTHER_VARIANT_;
+  double                cg_reduction        = 1.e-8;
+  unsigned              n_refinements       = 1;
+  unsigned              n_cycles            = 1;
+  unsigned              n_smoothing_steps   = 1;
+  unsigned              n_samples           = 10;
+  unsigned              n_subsamples_vmult  = 10;
+  unsigned              n_subsamples_smooth = 10;
+  unsigned              n_subsamples_mg     = 4;
 };
 
 std::string
-write_table_to_string(const Laplace::PostProcessData & pp_data, const Timings & timings)
+write_ppdata_to_string(const Laplace::PostProcessData & pp_data)
 {
   std::ostringstream oss;
 
+  ConvergenceTable info_table;
+  Assert(!pp_data.n_cells_global.empty(), ExcMessage("No cells to post process."));
+  for(unsigned run = 0; run < pp_data.n_cells_global.size(); ++run)
   {
-    ConvergenceTable info_table;
-    for(const auto item : pp_data.n_mg_levels)
-      info_table.add_value("n_levels", item);
-    for(const auto item : pp_data.n_cells_global)
-      info_table.add_value("n_cells", item);
-    for(const auto item : pp_data.n_dofs_global)
-      info_table.add_value("n_dofs", item);
-    for(const auto item : pp_data.n_colors_system)
-      info_table.add_value("n_colors", item);
-    for(const auto item : pp_data.n_iterations_system)
-      info_table.add_value("n_iter", item);
-    for(const auto item : pp_data.average_reduction_system)
-      info_table.add_value("reduction", item);
-
-    info_table.set_scientific("reduction", true);
-    info_table.set_precision("reduction", 3);
-    info_table.write_text(oss);
+    info_table.add_value("n_levels", pp_data.n_mg_levels.at(run));
+    info_table.add_value("n_cells", pp_data.n_cells_global.at(run));
+    info_table.add_value("n_dofs", pp_data.n_dofs_global.at(run));
+    info_table.add_value("n_colors", pp_data.n_colors_system.at(run));
+    info_table.add_value("n_iter", pp_data.n_iterations_system.at(run));
+    info_table.add_value("reduction", pp_data.average_reduction_system.at(run));
   }
 
-  oss << std::endl << std::endl;
+  info_table.set_scientific("reduction", true);
+  info_table.set_precision("reduction", 3);
+  info_table.write_text(oss);
 
+  return oss.str();
+}
+
+std::string
+write_timings_to_string(const Timings & timings)
+{
+  std::ostringstream oss;
+
+  ConvergenceTable timings_table;
+
+  for(unsigned n = 0; n < timings.apply.size(); ++n)
   {
-    ConvergenceTable timings_table;
-
-    for(unsigned n = 0; n < timings.apply.size(); ++n)
-    {
-      timings_table.add_value("sample", n);
-      timings_table.add_value("setup (max)", timings.setup[n].max);
-      timings_table.add_value("setup (min)", timings.setup[n].min);
-      timings_table.add_value("setup (avg)", timings.setup[n].avg);
-      timings_table.add_value("apply (max)", timings.apply[n].max);
-      timings_table.add_value("apply (min)", timings.apply[n].min);
-      timings_table.add_value("apply (avg)", timings.apply[n].avg);
-    }
-
-    timings_table.set_scientific("setup (max)", true);
-    timings_table.set_scientific("setup (min)", true);
-    timings_table.set_scientific("setup (avg)", true);
-    timings_table.set_scientific("apply (max)", true);
-    timings_table.set_scientific("apply (min)", true);
-    timings_table.set_scientific("apply (avg)", true);
-    timings_table.write_text(oss);
+    timings_table.add_value("sample", n + 1);
+    timings_table.add_value("setup (max)", timings.setup[n].max);
+    timings_table.add_value("setup (min)", timings.setup[n].min);
+    timings_table.add_value("setup (avg)", timings.setup[n].avg);
+    timings_table.add_value("apply (max)", timings.apply[n].max);
+    timings_table.add_value("apply (min)", timings.apply[n].min);
+    timings_table.add_value("apply (avg)", timings.apply[n].avg);
   }
+
+  timings_table.set_scientific("setup (max)", true);
+  timings_table.set_scientific("setup (min)", true);
+  timings_table.set_scientific("setup (avg)", true);
+  timings_table.set_scientific("apply (max)", true);
+  timings_table.set_scientific("apply (min)", true);
+  timings_table.set_scientific("apply (avg)", true);
+  timings_table.write_text(oss);
 
   return oss.str();
 }
 
 template<int dim, int fe_degree>
 void
-test(const TestParameter & prm = TestParameter{})
+test(const TestParameter & prms = TestParameter{})
 {
   constexpr int n_patch_dofs_per_direction =
     TPSS::UniversalInfo<dim>::n_cells_per_direction(CT::PATCH_VARIANT_) * (fe_degree + 1);
@@ -107,46 +106,52 @@ test(const TestParameter & prm = TestParameter{})
   oss << "_" << dim << "D";
   oss << "_" << fe_degree << "deg";
   oss << "_" << n_mpi_procs << "procs";
-  oss << "_" << prm.n_smoothing_steps << "steps";
+  oss << "_" << prms.n_smoothing_steps << "steps";
   // oss << std::scientific << std::setprecision(3);
-  // oss << "_" << prm.damping_factor << "ldamp";
+  // oss << "_" << prms.damping_factor << "ldamp";
   const std::string filename = oss.str();
 
   Laplace::Parameter parameters;
   //: discretization
-  parameters.n_cycles  = prm.n_cycles;
-  parameters.n_refines = prm.n_refinements;
+  parameters.n_cycles  = prms.n_cycles;
+  parameters.n_refines = prms.n_refinements;
   //: iterative solver
-  parameters.solver_reduction      = prm.cg_reduction;
+  parameters.solver_reduction      = prms.cg_reduction;
   parameters.solver_max_iterations = 100;
   parameters.precondition_variant  = Parameter::PreconditionVariant::MG;
   //: multigrid
   parameters.coarse_level                           = 1;
-  parameters.schwarz_smoother_data.patch_variant    = prm.patch_variant;
-  parameters.schwarz_smoother_data.smoother_variant = prm.smoother_variant;
+  parameters.schwarz_smoother_data.patch_variant    = prms.patch_variant;
+  parameters.schwarz_smoother_data.smoother_variant = prms.smoother_variant;
   parameters.schwarz_smoother_data.manual_coloring  = true;
   const double damping_factor =
-    TPSS::lookup_damping_factor(prm.patch_variant, prm.smoother_variant, dim);
+    TPSS::lookup_damping_factor(prms.patch_variant, prms.smoother_variant, dim);
   parameters.schwarz_smoother_data.damping_factor            = damping_factor;
-  parameters.schwarz_smoother_data.number_of_smoothing_steps = prm.n_smoothing_steps;
+  parameters.schwarz_smoother_data.number_of_smoothing_steps = prms.n_smoothing_steps;
   parameters.mg_smoother_post_reversed                       = true;
 
   Timings                  timings_vmult, timings_smooth, timings_mg, timings_total;
   Laplace::PostProcessData pp_data;
-  PoissonProblem poisson_problem{parameters};
+  PoissonProblem           poisson_problem{parameters};
   poisson_problem.create_triangulation(parameters.n_refines);
   poisson_problem.distribute_dofs();
 
-  for(unsigned sample = 0; sample < prm.n_samples; ++sample)
+  for(unsigned sample = 0; sample < prms.n_samples; ++sample)
   {
-    Timer          time(MPI_COMM_WORLD, true);
+    Timer time(MPI_COMM_WORLD, true);
 
     //: setup (total)
     time.restart();
     poisson_problem.prepare_linear_system();
     const auto & gmg_preconditioner = poisson_problem.prepare_preconditioner_mg();
     time.stop();
-    timings_total.setup.push_back(time.get_last_lap_wall_time_data());
+    timings_total.setup.push_back(time.get_accumulated_wall_time_data());
+
+    //: solve (total)
+    time.restart();
+    poisson_problem.solve(gmg_preconditioner);
+    time.stop();
+    timings_total.apply.push_back(time.get_last_lap_wall_time_data());
 
     {
       Timer time(MPI_COMM_WORLD, true);
@@ -165,10 +170,12 @@ test(const TestParameter & prm = TestParameter{})
       fill_with_random_values(tmp);
       VECTOR dst = tmp;
       time.restart();
-      for(unsigned subsample = 0; subsample < prm.n_subsamples_vmult; ++subsample)
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_vmult; ++subsample)
         system_matrix.vmult(dst, tmp);
       time.stop();
-      timings_vmult.apply.push_back(time.get_last_lap_wall_time_data());
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_vmult;
+      timings_vmult.apply.push_back(t_apply);
     }
 
     {
@@ -197,34 +204,61 @@ test(const TestParameter & prm = TestParameter{})
       fill_with_random_values(tmp);
       VECTOR dst = tmp;
       time.restart();
-      for(unsigned subsample = 0; subsample < prm.n_subsamples_smooth; ++subsample)
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_smooth; ++subsample)
         schwarz_smoother.step(dst, tmp);
       time.stop();
-      timings_smooth.apply.push_back(time.get_last_lap_wall_time_data());
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_vmult;
+      timings_smooth.apply.push_back(t_apply);
     }
 
-    time.restart();
-    poisson_problem.solve(gmg_preconditioner);
-    time.stop();
-    timings_total.apply.push_back(time.get_last_lap_wall_time_data());
+    {
+      Timer time(MPI_COMM_WORLD, true);
 
-    pp_data = poisson_problem.pp_data;
+      //: setup
+      time.restart();
+      const auto & gmg_preconditioner = poisson_problem.prepare_preconditioner_mg();
+      time.stop();
+      timings_mg.setup.push_back(time.get_last_lap_wall_time_data());
+
+      //: V-cycle
+      VECTOR tmp;
+      poisson_problem.system_matrix.get_matrix_free()->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      time.restart();
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_mg; ++subsample)
+        gmg_preconditioner.vmult(dst, tmp);
+      time.stop();
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_mg;
+      timings_mg.apply.push_back(t_apply);
+    }
   }
+
+  // {
+  //   PoissonProblem           poisson_problem{parameters};
+  //   poisson_problem.pcout
+  // }
 
   if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
   {
-    std::fstream fstream_log;
-    fstream_log.open("vmult_" + filename + ".time", std::ios_base::out); // std::ios_base::app);
-    fstream_log << write_table_to_string(pp_data, timings_vmult);
-    fstream_log.close();
+    std::fstream fstream;
+    fstream.open("vmult_" + filename + ".time", std::ios_base::out);
+    fstream << write_timings_to_string(timings_vmult);
+    fstream.close();
 
-    fstream_log.open("smooth_" + filename + ".time", std::ios_base::out); // std::ios_base::app);
-    fstream_log << write_table_to_string(pp_data, timings_smooth);
-    fstream_log.close();
+    fstream.open("smooth_" + filename + ".time", std::ios_base::out);
+    fstream << write_timings_to_string(timings_smooth);
+    fstream.close();
 
-    fstream_log.open("solve_" + filename + ".time", std::ios_base::out); // std::ios_base::app);
-    fstream_log << write_table_to_string(pp_data, timings_total);
-    fstream_log.close();
+    fstream.open("mg_" + filename + ".time", std::ios_base::out);
+    fstream << write_timings_to_string(timings_mg);
+    fstream.close();
+
+    fstream.open("solve_" + filename + ".time", std::ios_base::out);
+    fstream << write_timings_to_string(timings_total);
+    fstream.close();
   }
 }
 
