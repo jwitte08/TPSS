c65c1d4b20baf97a52b3d2c72a0a728b8038142f
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-11-15 08:53:49 +0100

    split partial() into vmult(), smooth() and mg()
    - memory leaks seem to be in multigrid structs
    - ..._raw() functions are without measured timings

--- apps/poisson_mpiscaling.cc
+++ apps/poisson_mpiscaling.cc
@@ -161,11 +161,10 @@ struct Test
 
   //: determine filename from test parameters
   std::string
-  get_filename(const PoissonProblem & poisson_problem)
+  get_filename(const unsigned int n_dofs_global)
   {
     std::ostringstream oss;
-    const auto         n_dofs_global = poisson_problem.pp_data.n_dofs_global.front();
-    const auto         n_mpi_procs   = Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);
+    const auto         n_mpi_procs = Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);
 
     oss << TPSS::getstr_schwarz_variant(CT::PATCH_VARIANT_, CT::SMOOTHER_VARIANT_);
     oss << "_" << dim << "D";
@@ -175,6 +174,11 @@ struct Test
     return oss.str();
   }
 
+  /*
+   * The memory stats in KByte. VmPeak and VmSize determines the peak and
+   * current virtual memory size (RAM + swap), whereas VmHWM and VmRSS
+   * determines the peak and current resident memory size (RAM).
+   */
   std::string
   str_memory_stats(std::string tag = "TBA")
   {
@@ -183,19 +187,18 @@ struct Test
     Utilities::System::get_memory_stats(memory_stats);
     unsigned long VmPeak_max =
       Utilities::MPI::max<unsigned long>(memory_stats.VmPeak, MPI_COMM_WORLD);
-    // oss << "VmPeak(" << tag << "): " << Util::si_metric_prefix(1000*VmPeak_max) << "Byte" <<
-    // std::endl;
-    oss << "VmPeak(" << tag << "): " << VmPeak_max << "KByte" << std::endl;
+    unsigned long VmSize_max =
+      Utilities::MPI::max<unsigned long>(memory_stats.VmSize, MPI_COMM_WORLD);
     unsigned long VmHWM_max =
       Utilities::MPI::max<unsigned long>(memory_stats.VmHWM, MPI_COMM_WORLD);
-    // oss << "VmHWM(" << tag << "): " << Util::si_metric_prefix(1000*VmHWM_max) << "Byte" <<
-    // std::endl;
-    oss << "VmHWM(" << tag << "): " << VmHWM_max << "KByte" << std::endl;
+    unsigned long VmRSS_max =
+      Utilities::MPI::max<unsigned long>(memory_stats.VmRSS, MPI_COMM_WORLD);
+    print_row(oss, 20, VmPeak_max, VmSize_max, VmHWM_max, VmRSS_max, "tag=" + tag);
     return oss.str();
   }
 
   /**
-   * test a single operator application, smoothing step and v-cycle
+   * Test single operator application, smoothing step and V-cycle
    */
   void
   partial()
@@ -210,6 +213,7 @@ struct Test
     for(unsigned sample = 0; sample < prms.n_samples; ++sample)
     {
       pcout << "Compute sample " << sample << " ..." << std::endl;
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
       pcout << str_memory_stats("initial");
       Timer time(MPI_COMM_WORLD, true);
 
@@ -317,7 +321,8 @@ struct Test
     } // end sample loop
 
     //: write performance timings
-    const std::string filename = get_filename(poisson_problem);
+    const auto        n_dofs_global = poisson_problem.pp_data.n_dofs_global.front();
+    const std::string filename      = get_filename(n_dofs_global);
     if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
     {
       std::fstream            fstream;
@@ -342,10 +347,10 @@ struct Test
   }
 
   /**
-   * run complete Poisson problem once writing a generic log-file
+   * Run complete Poisson problem and write a generic logfile.
    */
   void
-  full()
+  full(const bool once = true)
   {
     PoissonProblem     poisson_problem{rt_parameters};
     std::ostringstream oss;
@@ -353,17 +358,286 @@ struct Test
     poisson_problem.pcout            = std::make_shared<ConditionalOStream>(oss, is_first_proc);
     oss << write_header();
 
+    ConditionalOStream pcout(std::cout, is_first_proc);
     poisson_problem.run();
+    print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+    pcout << str_memory_stats("run");
     const auto pp_data = poisson_problem.pp_data;
     oss << write_ppdata_to_string(pp_data);
 
     if(is_first_proc)
     {
-      const std::string filename = get_filename(poisson_problem);
+      const auto        n_dofs_global = poisson_problem.pp_data.n_dofs_global.front();
+      const std::string filename      = get_filename(n_dofs_global);
       std::fstream      fstream;
       fstream.open("poisson_" + filename + ".log", std::ios_base::out);
       fstream << oss.str();
       fstream.close();
+    }
+
+    if(!once)
+    {
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+      for(unsigned sample = 1; sample < prms.n_samples; ++sample)
+      {
+        poisson_problem.run();
+        pcout << str_memory_stats("run");
+      }
+    }
+  }
+
+  void
+  vmult_raw()
+  {
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      const auto    mf_storage = poisson_problem.template build_mf_storage<double>();
+      SYSTEM_MATRIX system_matrix;
+      system_matrix.initialize(mf_storage);
+      //: vmult
+      VECTOR tmp;
+      mf_storage->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      system_matrix.vmult(dst, tmp);
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+      pcout << str_memory_stats("vmult_raw");
+    }
+  }
+
+  void
+  vmult()
+  {
+    Timings        timings_vmult;
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+
+    print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+    unsigned int n_dofs_global = 0;
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      Timer time(MPI_COMM_WORLD, true);
+      //:setup
+      time.restart();
+      const auto    mf_storage = poisson_problem.template build_mf_storage<double>();
+      SYSTEM_MATRIX system_matrix;
+      system_matrix.initialize(mf_storage);
+      n_dofs_global = system_matrix.m();
+      time.stop();
+      timings_vmult.setup.push_back(time.get_last_lap_wall_time_data());
+
+      //: vmult
+      VECTOR tmp;
+      mf_storage->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      time.restart();
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_vmult; ++subsample)
+        system_matrix.vmult(dst, tmp);
+      pcout << str_memory_stats("vmult");
+      time.stop();
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_vmult;
+      timings_vmult.apply.push_back(t_apply);
+    }
+
+    //: write performance timings
+    const std::string filename = get_filename(n_dofs_global);
+    if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+    {
+      std::fstream      fstream;
+      PostProcessData & pp_data = poisson_problem.pp_data;
+      pp_data.n_dofs_global.push_back(n_dofs_global);
+
+      fstream.open("vmult_" + filename + ".time", std::ios_base::out);
+      fstream << write_timings_to_string(timings_vmult, pp_data);
+      fstream.close();
+    }
+  }
+
+  void
+  smooth_raw()
+  {
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      //: setup
+      const unsigned fine_level = poisson_problem.triangulation.n_global_levels() - 1;
+      const auto     mf_storage = poisson_problem.template build_mf_storage<double>(fine_level);
+      LEVEL_MATRIX   level_matrix;
+      level_matrix.initialize(mf_storage);
+      const auto   subdomain_handler = poisson_problem.build_patch_storage(fine_level, mf_storage);
+      const auto & schwarz_data      = poisson_problem.rt_parameters.multigrid.pre_smoother.schwarz;
+      const auto   schwarz_preconditioner =
+        poisson_problem.build_schwarz_preconditioner(subdomain_handler, level_matrix, schwarz_data);
+      typename SCHWARZ_SMOOTHER::AdditionalData smoother_data;
+      smoother_data.number_of_smoothing_steps = schwarz_data.number_of_smoothing_steps;
+      SCHWARZ_SMOOTHER schwarz_smoother;
+      schwarz_smoother.initialize(level_matrix, schwarz_preconditioner, smoother_data);
+
+      //: smooth
+      VECTOR tmp;
+      mf_storage->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      schwarz_smoother.step(dst, tmp);
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+      pcout << str_memory_stats("smooth");
+    }
+  }
+
+  void
+  smooth()
+  {
+    Timings        timings_smooth;
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+    Timer time(MPI_COMM_WORLD, true);
+
+    print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+    unsigned int n_dofs_global = 0;
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      //: setup
+      time.restart();
+      const unsigned fine_level = poisson_problem.triangulation.n_global_levels() - 1;
+      const auto     mf_storage = poisson_problem.template build_mf_storage<double>(fine_level);
+      LEVEL_MATRIX   level_matrix;
+      level_matrix.initialize(mf_storage);
+      n_dofs_global                  = level_matrix.m();
+      const auto   subdomain_handler = poisson_problem.build_patch_storage(fine_level, mf_storage);
+      const auto & schwarz_data      = poisson_problem.rt_parameters.multigrid.pre_smoother.schwarz;
+      const auto   schwarz_preconditioner =
+        poisson_problem.build_schwarz_preconditioner(subdomain_handler, level_matrix, schwarz_data);
+      typename SCHWARZ_SMOOTHER::AdditionalData smoother_data;
+      smoother_data.number_of_smoothing_steps = schwarz_data.number_of_smoothing_steps;
+      SCHWARZ_SMOOTHER schwarz_smoother;
+      schwarz_smoother.initialize(level_matrix, schwarz_preconditioner, smoother_data);
+      time.stop();
+      timings_smooth.setup.push_back(time.get_last_lap_wall_time_data());
+
+      //: smooth
+      VECTOR tmp;
+      mf_storage->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      time.restart();
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_smooth; ++subsample)
+        schwarz_smoother.step(dst, tmp);
+      time.stop();
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_smooth;
+      timings_smooth.apply.push_back(t_apply);
+      pcout << str_memory_stats("smooth");
+    }
+
+    //: write performance timings
+    const std::string filename = get_filename(n_dofs_global);
+    if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+    {
+      std::fstream      fstream;
+      PostProcessData & pp_data = poisson_problem.pp_data;
+      pp_data.n_dofs_global.push_back(n_dofs_global);
+
+      fstream.open("smooth_" + filename + ".time", std::ios_base::out);
+      fstream << write_timings_to_string(timings_smooth, pp_data);
+      fstream.close();
+    }
+  }
+
+  void
+  mg_raw()
+  {
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+    poisson_problem.prepare_linear_system();
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      //: setup
+      const auto & gmg_preconditioner = poisson_problem.prepare_preconditioner_mg();
+
+      //: V-cycle
+      VECTOR tmp;
+      poisson_problem.system_matrix.get_matrix_free()->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      gmg_preconditioner.vmult(dst, tmp);
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+      pcout << str_memory_stats("mg");
+    }
+  }
+
+  void
+  mg()
+  {
+    Timings        timings_mg;
+    PoissonProblem poisson_problem{rt_parameters};
+    auto &         pcout = *(poisson_problem.pcout);
+    poisson_problem.create_triangulation(rt_parameters.mesh.n_refinements);
+    poisson_problem.distribute_dofs();
+    poisson_problem.prepare_linear_system();
+    Timer time(MPI_COMM_WORLD, true);
+    print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      //: setup
+      time.restart();
+      const auto & gmg_preconditioner = poisson_problem.prepare_preconditioner_mg();
+      time.stop();
+      timings_mg.setup.push_back(time.get_last_lap_wall_time_data());
+
+      //: V-cycle
+      VECTOR tmp;
+      poisson_problem.system_matrix.get_matrix_free()->initialize_dof_vector(tmp);
+      fill_with_random_values(tmp);
+      VECTOR dst = tmp;
+      time.restart();
+      for(unsigned subsample = 0; subsample < prms.n_subsamples_mg; ++subsample)
+        gmg_preconditioner.vmult(dst, tmp);
+      time.stop();
+      Utilities::MPI::MinMaxAvg t_apply = time.get_last_lap_wall_time_data();
+      t_apply                           = t_apply / prms.n_subsamples_mg;
+      timings_mg.apply.push_back(t_apply);
+      pcout << str_memory_stats("mg");
+    }
+
+    //: write performance timings
+    const auto        n_dofs_global = poisson_problem.pp_data.n_dofs_global.front();
+    const std::string filename      = get_filename(n_dofs_global);
+    if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+    {
+      std::fstream            fstream;
+      const PostProcessData & pp_data = poisson_problem.pp_data;
+      fstream.open("mg_" + filename + ".time", std::ios_base::out);
+      fstream << write_timings_to_string(timings_mg, pp_data);
+      fstream.close();
+    }
+  }
+
+  void
+  full_raw()
+  {
+    PoissonProblem poisson_problem{rt_parameters};
+
+    for(unsigned sample = 0; sample < prms.n_samples; ++sample)
+    {
+      poisson_problem.run();
+      auto & pcout = *(poisson_problem.pcout);
+      print_row(pcout, 20, "VmPeak", "VmSize", "VmHWM", "VmRSS");
+      pcout << str_memory_stats("run");
     }
   }
 };
@@ -392,19 +666,56 @@ main(int argc, char * argv[])
   std::ostringstream oss;
   Test<CT::DIMENSION_, CT::FE_DEGREE_> tester(prms);
 
-  time.restart();
-  pcout << Util::parameter_to_fstring("MF::vmult, TPSS::smooth & MG::vmult", "");
-  tester.partial();
+  // time.restart();
+  // pcout << Util::parameter_to_fstring("MF::vmult, TPSS::smooth & MG::vmult", "");
+  // tester.partial();
+  // time.stop();
+  // time.print_last_lap_wall_time_data(pcout);
+  // pcout << std::endl;
+
+  // time.start(); // checked: no leak!
+  // tester.vmult_raw();
+  // time.stop();
+
+  time.start();
+  tester.vmult();
   time.stop();
+  pcout << Util::parameter_to_fstring("Testing vmult()", "");
   time.print_last_lap_wall_time_data(pcout);
   pcout << std::endl;
 
+  // time.start(); // checked: no leak!
+  // tester.smooth_raw();
+  // time.stop();
+
   time.start();
-  tester.full();
+  tester.smooth();
   time.stop();
-  pcout << Util::parameter_to_fstring("Testing PoissonProblem::run()", "");
+  pcout << Util::parameter_to_fstring("Testing smooth()", "");
   time.print_last_lap_wall_time_data(pcout);
   pcout << std::endl;
+
+  // time.start();
+  // tester.mg_raw();
+  // time.stop();
+
+  time.start();
+  tester.mg(); // checked: leaks!
+  time.stop();
+  pcout << Util::parameter_to_fstring("Testing mg()", "");
+  time.print_last_lap_wall_time_data(pcout);
+  pcout << std::endl;
+
+  // time.start(); // checked: leaks!
+  // tester.full_raw();
+  // time.stop();
+
+  // time.start();
+  // tester.full(/*compute once or n samples?*/false); // checked: leaks!
+  // time.stop();
+  // pcout << Util::parameter_to_fstring("Testing PoissonProblem::run()", "");
+  // time.print_last_lap_wall_time_data(pcout);
+  // pcout << std::endl;
 
   pcout << Util::parameter_to_fstring("Total wall time elapsed", "");
   time.print_accumulated_wall_time_data(pcout);
