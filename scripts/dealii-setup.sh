0f3e5bc9affeb2adff8adbebeba70e1d945068f2
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-11-15 13:20:10 +0100

    deal.II setup with p4est

---
+++ scripts/dealii-setup.sh
@@ -0,0 +1,20 @@
+# remove old cmake files
+rm -rf CMakeFiles/ CMakeCache.txt
+
+# build configuration
+cmake \
+    -D CMAKE_CXX_FLAGS="-march=native -Wno-array-bounds -Wno-literal-suffix -pthread" \
+    -D DEAL_II_CXX_FLAGS_RELEASE="-O3 -funroll-all-loops" \
+    -D DEAL_II_CXX_FLAGS_DEBUG="-O0 -g" \
+    -D CMAKE_C_FLAGS="-march=native -Wno-array-bounds -Wfatal-errors" \
+    -D DEAL_II_WITH_MPI:BOOL="ON" \
+    -D DEAL_II_LINKER_FLAGS="-lpthread" \
+    -D DEAL_II_WITH_TRILINOS:BOOL="OFF" \
+    -D DEAL_II_FORCE_BUNDLED_BOOST="OFF" \
+    -D DEAL_II_WITH_GSL="OFF" \
+    -D DEAL_II_WITH_NETCDF="OFF" \
+    -D DEAL_II_WITH_P4EST="ON" \
+    -D DEAL_II_WITH_THREADS="ON" \
+    -D DEAL_II_COMPONENT_DOCUMENTATION="OFF" \
+    -D DEAL_II_COMPONENT_EXAMPLES="OFF" \
+    ..
