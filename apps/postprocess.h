e264a0746100051d75a9277889191eb0d3c06f64
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-11-20 09:48:16 +0100

    n_dofs_global set to 64bit type

--- apps/postprocess.h
+++ apps/postprocess.h
@@ -12,16 +12,16 @@
 
 struct PostProcessData
 {
-  std::vector<unsigned int> n_mg_levels;
-  std::vector<unsigned int> n_cells_global;
-  std::vector<unsigned int> n_dofs_global;
-  std::vector<unsigned int> n_colors_system;
-  std::vector<double>       solve_time;
-  std::vector<double>       setup_time;
-  std::vector<double>       n_iterations_system;
-  std::vector<double>       average_reduction_system;
-  std::vector<double>       L2_error;
-  std::vector<double>       H1semi_error;
+  std::vector<unsigned int>            n_mg_levels;
+  std::vector<unsigned int>            n_cells_global;
+  std::vector<types::global_dof_index> n_dofs_global;
+  std::vector<unsigned int>            n_colors_system;
+  std::vector<double>                  solve_time;
+  std::vector<double>                  setup_time;
+  std::vector<double>                  n_iterations_system;
+  std::vector<double>                  average_reduction_system;
+  std::vector<double>                  L2_error;
+  std::vector<double>                  H1semi_error;
 };
 
 #endif /* POSTPROCESS_H_ */
