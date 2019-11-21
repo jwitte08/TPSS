0f892991389b67e0fd1cd76f54edb72d4e21fb98
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-11-21 15:32:52 +0100

    lookup_solver_variant()
    - dependent on the pre- and post-smoothing settings we decide if the PCG
    or preconditioned GMRES solver is chosen

--- apps/rt_parameter.h
+++ apps/rt_parameter.h
@@ -31,6 +31,9 @@ struct SolverParameter
   };
   static std::string
   str_precondition_variant(const PreconditionVariant variant);
+  static std::string
+  lookup_solver_variant(const SmootherParameter pre_smoother,
+                        const SmootherParameter post_smoother);
 
   std::string         variant              = "none"; // see SolverSelector
   double              abs_tolerance        = 1.e-14;
@@ -38,9 +41,14 @@ struct SolverParameter
   int                 n_iterations_max     = 100;
   PreconditionVariant precondition_variant = PreconditionVariant::None;
 
+  void
+  set_solver_variant(const SmootherParameter pre_smoother, const SmootherParameter post_smoother);
+
   std::string
   to_string() const;
 };
+
+
 
 namespace RT
 {
@@ -66,6 +74,46 @@ SolverParameter::str_precondition_variant(const PreconditionVariant variant)
   return str_variant[(int)variant];
 }
 
+
+std::string
+SolverParameter::lookup_solver_variant(const SmootherParameter pre_smoother,
+                                       const SmootherParameter post_smoother)
+{
+  std::string variant    = "";
+  const bool  is_schwarz = pre_smoother.variant == SmootherParameter::SmootherVariant::Schwarz &&
+                          post_smoother.variant == SmootherParameter::SmootherVariant::Schwarz;
+  AssertThrow(is_schwarz, ExcMessage("TODO"));
+  if(is_schwarz)
+  {
+    const bool pre_smoother_is_additive =
+      pre_smoother.schwarz.smoother_variant == TPSS::SmootherVariant::additive;
+    const bool pre_smoother_is_symmetric =
+      pre_smoother_is_additive || pre_smoother.schwarz.symmetrize_smoothing;
+    const bool post_smoother_is_additive =
+      post_smoother.schwarz.smoother_variant == TPSS::SmootherVariant::additive;
+    const bool post_smoother_is_symmetric =
+      post_smoother_is_additive || post_smoother.schwarz.symmetrize_smoothing;
+    const bool smoothers_are_symmetric = pre_smoother_is_symmetric && post_smoother_is_symmetric;
+    const bool vcycle_is_symmetric =
+      (pre_smoother.schwarz.reverse_smoothing && !post_smoother.schwarz.reverse_smoothing) ||
+      (!pre_smoother.schwarz.reverse_smoothing && post_smoother.schwarz.reverse_smoothing);
+    if(smoothers_are_symmetric || vcycle_is_symmetric)
+      variant = "cg";
+    else
+      variant = "gmres";
+  }
+  return variant;
+}
+
+
+void
+SolverParameter::set_solver_variant(const SmootherParameter pre_smoother,
+                                    const SmootherParameter post_smoother)
+{
+  variant = lookup_solver_variant(pre_smoother, post_smoother);
+}
+
+
 std::string
 SolverParameter::to_string() const
 {
@@ -78,6 +126,7 @@ SolverParameter::to_string() const
                                     str_precondition_variant(precondition_variant));
   return oss.str();
 }
+
 
 namespace RT
 {
