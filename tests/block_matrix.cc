f7ce409618fc05406208588f037a269b84e6fafd
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2019-12-19 09:31:13 +0100

    Test smaller units of SchurComplementFast

--- tests/block_matrix.cc
+++ tests/block_matrix.cc
@@ -62,7 +62,8 @@ protected:
     matrix,
     inverse,
     schur,
-    inverse_schur
+    inverse_schur,
+    schur_fast
   };
 
   static constexpr int dim                 = T::template type<0>::template value<0>();
@@ -94,6 +95,12 @@ protected:
                          const FullMatrix<Number> & other)
   {
     Util::compare_inverse_matrix(inverse_matrix, other, *pcout_owned);
+  }
+
+  void
+  compare_vector(const Vector<Number> & vector, const Vector<Number> & other)
+  {
+    Util::compare_vector(vector, other, *pcout_owned);
   }
 
   void
@@ -263,11 +270,188 @@ TYPED_TEST_P(FixBlockMatrixVmult, CompareInverse)
   Fixture::do_vectorized_test(Fixture::TestVariant::inverse);
 }
 
+TYPED_TEST_P(FixBlockMatrixVmult, CompareSchurFastBlockDiagonal)
+{
+  using Fixture            = FixBlockMatrixVmult<TypeParam>;
+  static constexpr int dim = TypeParam::template type<0>::template value<0>();
+  using Number             = typename TypeParam::template type<1>;
+  using value_type         = VectorizedArray<Number>;
+
+  const unsigned int   m = 3;
+  Table<2, value_type> zero(m, m), id(m, m);
+  zero.fill(static_cast<value_type>(0.));
+  id = zero;
+  for(auto i = 0U; i < m; ++i)
+    id(i, i) = static_cast<value_type>(1.);
+
+  const auto assemble_zero_tensor = [zero]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    std::fill(tensor.begin(), tensor.end(), zero);
+    return tensor;
+  };
+  const auto assemble_id_tensor = [id]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    std::fill(tensor.begin(), tensor.end(), id);
+    return tensor;
+  };
+  const auto assemble_random_tensor = [m]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    for(auto & matrix : tensor)
+      fill_matrix_with_random_values(matrix, m, m);
+    std::transform(tensor.cbegin(), tensor.cend(), tensor.begin(), [](const auto & A) {
+      return Tensors::sum(A, Tensors::transpose(A));
+    });
+    return tensor;
+  };
+
+  std::vector<std::array<Table<2, value_type>, dim>> A, B, C, D;
+  B.emplace_back(assemble_zero_tensor());
+  C.emplace_back(assemble_zero_tensor());
+  A.emplace_back(assemble_id_tensor());
+  A.emplace_back(assemble_random_tensor());
+  D.emplace_back(assemble_id_tensor());
+  D.emplace_back(assemble_random_tensor());
+  using State = typename Tensors::TensorProductMatrix<dim, value_type>::State;
+  Tensors::TensorProductMatrix<dim, value_type> AA(A, State::skd), BB(B), CC(C), DD(D, State::skd);
+
+  /// compare the fast diagonalized Schur complement
+  Tensors::SchurComplementFast<dim, value_type, 2>                        schur_fd(AA, BB, CC, DD);
+  Tensors::SchurComplement<Tensors::TensorProductMatrix<dim, value_type>> schur(AA, BB, CC, DD);
+
+  for(auto lane = 0U; lane < get_macro_size<value_type>(); ++lane)
+  {
+    const auto & S_fd    = table_to_fullmatrix(schur_fd.as_table(), lane);
+    const auto & Sinv_fd = table_to_fullmatrix(schur_fd.as_inverse_table(), lane);
+    const auto & S       = table_to_fullmatrix(schur.as_table(), lane);
+    Fixture::compare_matrix(S_fd, S);
+    Fixture::compare_inverse_matrix(Sinv_fd, S);
+  }
+}
+
+TYPED_TEST_P(FixBlockMatrixVmult, CompareSchurFastEigenvalueKSVD)
+{
+  using Fixture            = FixBlockMatrixVmult<TypeParam>;
+  static constexpr int dim = TypeParam::template type<0>::template value<0>();
+  using Number             = typename TypeParam::template type<1>;
+  using value_type         = VectorizedArray<Number>;
+  using State              = typename Tensors::TensorProductMatrix<dim, value_type>::State;
+
+  constexpr unsigned int m = 3;
+  Table<2, value_type>   zero(m, m), id(m, m);
+  zero.fill(static_cast<value_type>(0.));
+  id = zero;
+  for(auto i = 0U; i < m; ++i)
+    id(i, i) = static_cast<value_type>(1.);
+
+  const auto assemble_zero_tensor = [zero]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    std::fill(tensor.begin(), tensor.end(), zero);
+    return tensor;
+  };
+  const auto assemble_id_tensor = [id]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    std::fill(tensor.begin(), tensor.end(), id);
+    return tensor;
+  };
+  const auto assemble_random_tensor = [m]() {
+    std::array<Table<2, value_type>, dim> tensor;
+    for(auto & matrix : tensor)
+      fill_matrix_with_random_values(matrix, m, m);
+    std::transform(tensor.cbegin(), tensor.cend(), tensor.begin(), [](const auto & A) {
+      return Tensors::sum(A, Tensors::transpose(A));
+    });
+    return tensor;
+  };
+
+  std::vector<std::array<Table<2, value_type>, dim>> A;
+  A.emplace_back(assemble_id_tensor());
+  A.emplace_back(assemble_random_tensor());
+  Tensors::TensorProductMatrix<dim, value_type> AA(A, State::skd);
+
+  /// compare inverse eigenvalues with the associated KSVD
+  constexpr unsigned int    rank        = m;
+  auto                      eigenvalues = AA.get_eigenvalues();
+  AlignedVector<value_type> inverse_eigenvalues(eigenvalues.size());
+  std::transform(eigenvalues.begin(),
+                 eigenvalues.end(),
+                 inverse_eigenvalues.begin(),
+                 [](const auto & lambda) { return static_cast<value_type>(1. / lambda); });
+
+  std::array<std::array<Table<2, value_type>, dim>, rank> ksvd_eigenvalues_;
+  for(auto & tensor : ksvd_eigenvalues_)
+    for(auto d = 0U; d < dim; ++d)
+      tensor[d].reinit(AA.m(d), AA.m(d));
+  compute_ksvd_reverse<dim, value_type, rank>(inverse_eigenvalues, ksvd_eigenvalues_);
+  std::vector<std::array<Table<2, value_type>, dim>> ksvd_eigenvalues;
+  std::copy(ksvd_eigenvalues_.cbegin(),
+            ksvd_eigenvalues_.cend(),
+            std::back_inserter(ksvd_eigenvalues));
+  Tensors::TensorProductMatrix<dim, value_type> Lambda(ksvd_eigenvalues);
+  for(auto lane = 0U; lane < get_macro_size<value_type>(); ++lane)
+  {
+    const auto         Lambda_full = table_to_fullmatrix(Lambda.as_table(), lane);
+    FullMatrix<Number> Lambda_reference(Lambda.m());
+    for(auto i = 0U; i < Lambda_reference.m(); ++i)
+      Lambda_reference(i, i) = inverse_eigenvalues[i][lane];
+    Fixture::compare_matrix(Lambda_full, Lambda_reference);
+  }
+
+  std::vector<std::array<Table<2, value_type>, dim>> eigenvectors(1), eigenvectorsT(1);
+  eigenvectors.front() = AA.get_eigenvectors();
+  std::transform(eigenvectors.front().cbegin(),
+                 eigenvectors.front().cend(),
+                 eigenvectorsT.front().begin(),
+                 [](const auto & tab) { return Tensors::transpose(tab); });
+
+  /// check eigenvectors
+  Tensors::TensorProductMatrix<dim, value_type> Q(eigenvectors);
+  for(auto lane = 0U; lane < get_macro_size<value_type>(); ++lane)
+  {
+    const auto Q_full  = table_to_fullmatrix(Q.as_table(), lane);
+    const auto AA_full = table_to_fullmatrix(AA.as_table(), lane);
+    for(auto col = 0U; col < AA.n(); ++col)
+    {
+      Vector<Number> u(Q_full.m()), v(AA.m());
+      for(auto i = 0U; i < u.size(); ++i)
+        u[i] = Q_full(i, col);
+      AA_full.vmult(v, u);
+      v /= eigenvalues[col][lane];
+      Fixture::compare_vector(u, v);
+    }
+  }
+
+  /// compare reconstructed A^-1 with original A
+  const auto Lambda_x_QT     = Tensors::product<dim, value_type>(ksvd_eigenvalues, eigenvectorsT);
+  const auto Q_x_Lambda_x_QT = Tensors::product<dim, value_type>(eigenvectors, Lambda_x_QT);
+  Tensors::TensorProductMatrix<dim, value_type> AA_reconstructed(Q_x_Lambda_x_QT);
+  for(auto lane = 0U; lane < get_macro_size<value_type>(); ++lane)
+  {
+    const auto AA_inverse      = table_to_fullmatrix(AA_reconstructed.as_table(), lane);
+    const auto AA_reference    = table_to_fullmatrix(AA.as_table(), lane);
+    const auto AA_invreference = table_to_fullmatrix(AA.as_inverse_table(), lane);
+    Fixture::compare_matrix(AA_inverse, AA_invreference);
+    Fixture::compare_inverse_matrix(AA_inverse, AA_reference);
+  }
+
+  // for (auto lane = 0U; lane < get_macro_size<value_type>(); ++lane)
+  //   {
+  //     const auto & Sinv_fd = table_to_fullmatrix(schur_fd.as_inverse_table(), lane);
+  //     const auto & S = table_to_fullmatrix(schur.as_table(), lane);
+  //     FullMatrix<Number> ID(IdentityMatrix(S.m())), Diff(S.m(), S.n());
+  //     Sinv_fd.mmult(Diff, S);
+  //     Diff.print_formatted(std::cout);
+  //     Diff.add(-1., ID);
+  //     std::cout << "Deviation: " << Diff.frobenius_norm()/(Diff.m()*Diff.n()) << std::endl;
+  //   }
+}
+
 REGISTER_TYPED_TEST_SUITE_P(FixBlockMatrixVmult,
                             CompareMatrix,
                             CompareSchur,
                             CompareInverseSchur,
-                            CompareInverse);
+                            CompareInverse,
+                            CompareSchurFastBlockDiagonal,
+                            CompareSchurFastEigenvalueKSVD);
 
 using ParamsTwoDimensionsDouble = testing::Types<Util::TypeList<Util::NonTypeParams<2>, double>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(TwoDimensionsDouble, FixBlockMatrixVmult, ParamsTwoDimensionsDouble);
