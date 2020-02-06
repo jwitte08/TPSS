d55a5336392362700a0919b25fd75e946b902077
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2020-02-06 14:37:50 +0100

    create_triangulation()

--- apps/poisson_standard.h
+++ apps/poisson_standard.h
@@ -286,6 +286,16 @@ struct ModelProblem : public Subscriptor
   }
 
 
+  /*
+   * Convenience function creating the mesh based on RT::Parameter.
+   */
+  bool
+  create_triangulation()
+  {
+    return create_triangulation(rt_parameters.mesh.n_refinements);
+  }
+
+
   void
   distribute_dofs()
   {
