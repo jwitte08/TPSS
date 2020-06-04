7b6e651c8cdfe687e7c1705f5d9f073978222b2e
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2020-06-04 12:58:39 +0200

    namespace

--- apps/coloring.h
+++ apps/coloring.h
@@ -1,3 +1,5 @@
+#ifndef APPS_COLORING_H
+#define APPS_COLORING_H
 
 /*
  * user-defined coloring algorithms:
@@ -9,9 +11,6 @@
  *  Created on: Sep 20, 2019
  *      Author: witte
  */
-
-#ifndef TPSS_COLORING_H
-#define TPSS_COLORING_H
 
 #include <deal.II/base/conditional_ostream.h>
 #include <deal.II/base/index_set.h>
@@ -41,8 +40,6 @@
 
 #include "mesh.h"
 
-using namespace dealii;
-
 
 
 namespace Impl
@@ -50,68 +47,69 @@ namespace Impl
 template<int dim>
 void
 visualize_coloring(
-  const DoFHandler<dim> &                                                        dof_handler_in,
+  const dealii::DoFHandler<dim> &                                                dof_handler_in,
   const std::vector<std::vector<typename TPSS::PatchInfo<dim>::PatchIterator>> & colored_iterators,
   const std::string                                                              prefix)
 {
   for(unsigned color = 0; color < colored_iterators.size(); ++color)
   {
     // collect output data
-    const auto &       tria         = dof_handler_in.get_triangulation();
-    const unsigned int global_level = tria.n_global_levels() - 1;
-    DoFHandler<dim>    dof_handler(tria);
+    const auto &            tria         = dof_handler_in.get_triangulation();
+    const unsigned int      global_level = tria.n_global_levels() - 1;
+    dealii::DoFHandler<dim> dof_handler(tria);
     dof_handler.distribute_dofs(FE_DGQ<dim>(0)); // require one (dof) value per cell !!!
     dof_handler.distribute_mg_dofs();
-    DataOut<dim> data_out;
+    dealii::DataOut<dim> data_out;
     data_out.attach_dof_handler(dof_handler);
 
     // print the subdomain_ids
-    Vector<double> subdomain(tria.n_active_cells());
+    dealii::Vector<double> subdomain(tria.n_active_cells());
     for(unsigned int i = 0; i < subdomain.size(); ++i)
       subdomain(i) = tria.locally_owned_subdomain();
     data_out.add_data_vector(subdomain, "subdomain", DataOut<dim>::type_cell_data);
 
     // initialize the ghosted dof vector
-    LinearAlgebra::distributed::Vector<double> marker;
-    IndexSet                                   locally_relevant_dofs;
-    DoFTools::extract_locally_relevant_level_dofs(dof_handler, global_level, locally_relevant_dofs);
+    dealii::LinearAlgebra::distributed::Vector<double> marker;
+    IndexSet                                           locally_relevant_dofs;
+    dealii::DoFTools::extract_locally_relevant_level_dofs(dof_handler,
+                                                          global_level,
+                                                          locally_relevant_dofs);
     marker.reinit(dof_handler.locally_owned_dofs(), locally_relevant_dofs, MPI_COMM_WORLD);
     marker                        = 0;
     const auto & patch_collection = colored_iterators[color];
     for(const auto & patch : patch_collection)
       for(const auto & cell : (*patch))
       {
-        const auto active_cell = typename DoFHandler<dim>::active_cell_iterator{&tria,
-                                                                                cell->level(),
-                                                                                cell->index(),
-                                                                                &dof_handler};
+        const auto active_cell = typename dealii::DoFHandler<dim>::active_cell_iterator{
+          &tria, cell->level(), cell->index(), &dof_handler};
         std::vector<types::global_dof_index> dof_indices(dof_handler.get_fe().dofs_per_cell);
         active_cell->get_dof_indices(dof_indices);
         const auto active_cell_index = dof_indices.front();
         marker(active_cell_index)    = 1;
       }
-    marker.compress(VectorOperation::add);
-    data_out.add_data_vector(marker, "coloring", DataOut<dim>::type_dof_data);
+    marker.compress(dealii::VectorOperation::add);
+    data_out.add_data_vector(marker, "coloring", dealii::DataOut<dim>::type_dof_data);
 
     // write paraview files
-    std::string color_name = "_color" + Utilities::int_to_string(color, 2);
+    std::string color_name = "_color" + dealii::Utilities::int_to_string(color, 2);
     data_out.build_patches();
     {
       std::ofstream file(
-        prefix + "data-active-" + Utilities::int_to_string(dim) + "d-" +
-        Utilities::int_to_string(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD), 4) + color_name +
-        ".vtu");
+        prefix + "data-active-" + dealii::Utilities::int_to_string(dim) + "d-" +
+        dealii::Utilities::int_to_string(dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD),
+                                         4) +
+        color_name + ".vtu");
       data_out.write_vtu(file);
     }
-    if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
+    if(dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
     {
       std::vector<std::string> filenames;
-      for(unsigned int i = 0; i < Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD); ++i)
-        filenames.push_back(prefix + "data-active-" + Utilities::int_to_string(dim) + "d-" +
-                            Utilities::int_to_string(i, 4) + color_name + ".vtu");
+      for(unsigned int i = 0; i < dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD); ++i)
+        filenames.push_back(prefix + "data-active-" + dealii::Utilities::int_to_string(dim) + "d-" +
+                            dealii::Utilities::int_to_string(i, 4) + color_name + ".vtu");
 
-      std::ofstream master_output(prefix + "data-active-" + Utilities::int_to_string(dim) + "d" +
-                                  color_name + ".pvtu");
+      std::ofstream master_output(prefix + "data-active-" + dealii::Utilities::int_to_string(dim) +
+                                  "d" + color_name + ".pvtu");
       data_out.write_pvtu_record(master_output, filenames);
     }
   }
@@ -130,7 +128,7 @@ struct IntegerCoordinate
   }
 
   Point<dim, unsigned int>
-  operator()(const CellId cell_id) const
+  operator()(const dealii::CellId cell_id) const
   {
     // Extract child indices from CellId
     std::vector<unsigned int> child_indices;
@@ -144,9 +142,9 @@ struct IntegerCoordinate
     }
 
     // Compute integer coordinates of root cells
-    Point<dim, unsigned int> global_coord;
-    const unsigned int       root_cell_index = cell_id.to_binary<dim>()[0];
-    global_coord                             = integer_coordinate_root(root_cell_index);
+    dealii::Point<dim, unsigned int> global_coord;
+    const unsigned int               root_cell_index = cell_id.to_binary<dim>()[0];
+    global_coord                                     = integer_coordinate_root(root_cell_index);
 
     // Compute global integer coordinates by traversing through children cells.  We assume the
     // refinement is uniform, namely dividing each dimension by two.
@@ -215,7 +213,7 @@ struct ColoringBase
 
 
   static void
-  visualize_coloring(const DoFHandler<dim> & dof_handler_in,
+  visualize_coloring(const dealii::DoFHandler<dim> & dof_handler_in,
                      const std::vector<std::vector<typename TPSS::PatchInfo<dim>::PatchIterator>> &
                                        colored_iterators,
                      const std::string prefix)
@@ -251,7 +249,7 @@ struct RedBlackColoring : public ColoringBase<dim>
       const auto & cell = patch->front();
 
       // Get integer coordinates
-      Point<dim, unsigned int> cell_int_coords = this->get_integer_coordinate(cell->id());
+      dealii::Point<dim, unsigned int> cell_int_coords = this->get_integer_coordinate(cell->id());
 
       // If integer coordinates sum to an even
       // number give color 0, else give color 1
@@ -296,9 +294,9 @@ struct RedBlackColoring : public ColoringBase<dim>
       AssertIndexRange(shift_mask.to_ulong(), 1 << dim);
 
       // (1 << dim) layers of red-black colorings (layer is determined by the shift)
-      const auto                     cell        = patch->front();
-      const Point<dim, unsigned int> coordinates = this->get_integer_coordinate(cell->id());
-      unsigned int                   sum         = 0;
+      const auto                             cell        = patch->front();
+      const dealii::Point<dim, unsigned int> coordinates = this->get_integer_coordinate(cell->id());
+      unsigned int                           sum         = 0;
       for(unsigned int d = 0; d < dim; ++d)
         sum += (coordinates(d) + static_cast<unsigned>(shift_mask[d])) / 2;
       const unsigned red_or_black = sum % 2;
@@ -375,4 +373,4 @@ struct TiledColoring : public ColoringBase<dim>
 
 
 
-#endif /* TPSS_COLORING_H */
+#endif /* APPS_COLORING_H */
