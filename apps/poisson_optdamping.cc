3b00533eceb2b1c4b339d671952daf35b88d828a
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2020-12-02 12:14:26 +0100

    POISSON optimal smoothing AVP DGQ
    1) compensate overlap by damping w_ov
    2) nonsymmetric spectrum around 0, thus, compute damping w_opt for
    w_ov-damped block-Jacobi solver: w_opt = 2/(2-lambda_min-lambda_max)
    3) shift spectrum to smooth high to mid-high frequencies: 0.7w_opt

--- apps/poisson_optdamping.cc
+++ apps/poisson_optdamping.cc
@@ -90,6 +90,100 @@ struct SubmatrixHandler
   unsigned int                         current_lane;
 };
 
+
+
+template<typename MatrixType, typename PrecondType>
+struct ErrorPropagationMatrix
+{
+  using value_type = typename MatrixType::value_type;
+
+  ErrorPropagationMatrix(const MatrixType & matrix_in, const PrecondType & precond_in)
+    : matrix(matrix_in), precond(precond_in)
+  {
+  }
+
+  types::global_dof_index
+  m() const
+  {
+    return matrix.m();
+  }
+
+  types::global_dof_index
+  n() const
+  {
+    return matrix.n();
+  }
+
+  void
+  vmult(const ArrayView<value_type> dst_view, const ArrayView<const value_type> src_view) const
+  {
+    LinearAlgebra::distributed::Vector<value_type> dst, src, Ax;
+    matrix.initialize_dof_vector(dst);
+    matrix.initialize_dof_vector(src);
+    matrix.initialize_dof_vector(Ax);
+
+    std::copy(src_view.cbegin(), src_view.cend(), src.begin());
+    matrix.vmult(Ax, src);  // Ax
+    precond.vmult(dst, Ax); // B^{-1}Ax
+    dst.sadd(-1., 1., src); // (I-B^{-1}A)x
+    std::copy(dst.begin(), dst.end(), dst_view.begin());
+  }
+
+  FullMatrix<value_type>
+  as_fullmatrix()
+  {
+    return table_to_fullmatrix(Tensors::matrix_to_table(*this));
+  }
+
+  const MatrixType &  matrix;
+  const PrecondType & precond;
+};
+
+
+
+template<typename MatrixType>
+struct MatrixWrapper
+{
+  using value_type = typename MatrixType::value_type;
+
+  MatrixWrapper(const MatrixType & matrix_in) : matrix(matrix_in)
+  {
+  }
+
+  types::global_dof_index
+  m() const
+  {
+    return matrix.m();
+  }
+
+  types::global_dof_index
+  n() const
+  {
+    return matrix.n();
+  }
+
+  void
+  vmult(const ArrayView<value_type> dst_view, const ArrayView<const value_type> src_view) const
+  {
+    LinearAlgebra::distributed::Vector<value_type> dst(dst_view.size());
+    LinearAlgebra::distributed::Vector<value_type> src(src_view.size());
+
+    std::copy(src_view.cbegin(), src_view.cend(), src.begin());
+    matrix.vmult(dst, src);
+    std::copy(dst.begin(), dst.end(), dst_view.begin());
+  }
+
+  FullMatrix<value_type>
+  as_fullmatrix()
+  {
+    return table_to_fullmatrix(Tensors::matrix_to_table(*this));
+  }
+
+  const MatrixType & matrix;
+};
+
+
+
 int
 main(int argc, char * argv[])
 {
@@ -109,7 +203,7 @@ main(int argc, char * argv[])
   //: default
   unsigned n_refinements = 0;
   unsigned n_repetitions = 3;
-  double   damping       = 0.;
+  double   damping       = 1.;
   int      n_threads_max = 1;
 
   //: parse runtime arguments
@@ -141,7 +235,7 @@ main(int argc, char * argv[])
     rt_parameters.multigrid.pre_smoother.schwarz.userdefined_coloring = true;
     rt_parameters.multigrid.pre_smoother.schwarz.damping_factor       = damping;
     rt_parameters.multigrid.post_smoother = rt_parameters.multigrid.pre_smoother;
-    rt_parameters.reset_damping_factor(dim);
+    // rt_parameters.reset_damping_factor(dim);
   }
 
   using PoissonProblem = typename Poisson::ModelProblem<dim, fe_degree, dof_layout, double>;
@@ -160,14 +254,30 @@ main(int argc, char * argv[])
 
   AssertThrow(poisson_problem->mg_schwarz_smoother_pre, ExcMessage("Check runtime parameters."));
 
-  const auto   level           = poisson_problem->mg_matrices.max_level();
-  const auto & level_matrix    = poisson_problem->mg_matrices[level];
+  const auto   level        = poisson_problem->mg_matrices.max_level();
+  const auto & level_matrix = poisson_problem->mg_matrices[level];
+
   const auto subdomain_handler = poisson_problem->mg_schwarz_smoother_pre->get_subdomain_handler();
   SubmatrixHandler submatrix_handler(level_matrix, *subdomain_handler); // R_j A_l R_j^T
   const auto &     patch_dof_worker = submatrix_handler.left_transfer->get_patch_dof_worker();
+  const auto & level_precond = *poisson_problem->mg_schwarz_smoother_pre->get_preconditioner(level);
+  const auto & local_solvers = *(level_precond.get_local_solvers());
 
-  const auto & local_solvers =
-    *poisson_problem->mg_schwarz_smoother_pre->get_preconditioner(level)->get_local_solvers();
+  AssertThrow(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1U,
+              ExcMessage("MPI not supported."));
+
+  const bool print_details =
+    poisson_problem->triangulation.n_global_active_cells() < 20 && dim == 2;
+
+  const auto & print_fullmatrix = [&](const FullMatrix<double> & matrix,
+                                      const std::string &        description) {
+    if(!print_details)
+      return;
+
+    std::cout << description << std::endl;
+    matrix.print_formatted(std::cout);
+    std::cout << std::endl;
+  };
 
   for(auto j = 0U; j < patch_dof_worker.n_subdomains(); ++j)
   {
@@ -175,19 +285,112 @@ main(int argc, char * argv[])
     for(auto lane = 0U; lane < patch_dof_worker.n_lanes_filled(j); ++lane)
     {
       const auto & Aj = table_to_fullmatrix(Tensors::matrix_to_table(local_solver), lane);
-      if(pcout->is_active())
-      {
-        *pcout << "Aj:" << std::endl;
-        Aj.print_formatted(pcout->get_stream());
-      }
+      print_fullmatrix(Aj, "Aj:");
 
       submatrix_handler.reinit(j, j, lane);
       const auto & local_matrix =
         table_to_fullmatrix(Tensors::matrix_to_table(submatrix_handler), lane);
-      if(pcout->is_active())
+      print_fullmatrix(local_matrix, "Rj A Rj^T:");
+    }
+  }
+
+  MatrixWrapper matrix_wrap(level_matrix);
+  const auto &  level_fullmatrix = matrix_wrap.as_fullmatrix();
+  print_fullmatrix(level_fullmatrix, "level matrix A:");
+
+  MatrixWrapper precond_wrap(level_precond);
+  const auto &  level_fullprecond = precond_wrap.as_fullmatrix();
+  print_fullmatrix(level_fullprecond, "preconditioner B^{-1}:");
+
+  FullMatrix<double> BinvA(level_fullmatrix.m());
+  level_fullprecond.mmult(BinvA, level_fullmatrix);
+  print_fullmatrix(BinvA, "B^{-1}A:");
+
+  {
+    FullMatrix<double> ABinvA(level_fullmatrix.m());
+    level_fullmatrix.mmult(ABinvA, BinvA);
+    FullMatrix<double> Q(level_fullmatrix.m());
+    const auto & geigenvalues = compute_generalized_eigenvalues_symm(ABinvA, level_fullmatrix, Q);
+    std::cout
+      << "generalized eigenvalues for AB^{-1}Ax = Ax, i.e. eigenvalues of B^{-1}A in A-induced inner product:"
+      << std::endl;
+    std::cout << vector_to_string(geigenvalues) << std::endl;
+    std::cout << std::endl;
+
+    /// DEBUG
+    // FullMatrix<double> tmp(level_fullmatrix.m());
+    // FullMatrix<double> tmp2(level_fullmatrix.m());
+    // ABinvA.mmult(tmp, Q);
+    // Q.Tmmult(tmp2, tmp);
+    // tmp2.print_formatted(std::cout);
+    // std::cout << std::endl;
+  }
+
+  FullMatrix<double> E(IdentityMatrix(level_fullmatrix.m()));
+  E.add(-1., BinvA);
+  print_fullmatrix(E, "error propagation matrix E=I-B^{-1}A:");
+
+  {
+    FullMatrix<double> AE(level_fullmatrix.m());
+    level_fullmatrix.mmult(AE, E);
+    FullMatrix<double> Q(level_fullmatrix.m());
+    const auto &       geigenvalues = compute_generalized_eigenvalues_symm(AE, level_fullmatrix, Q);
+    std::cout
+      << "generalized eigenvalues for AEx = Ax, i.e. eigenvalues of error propagation E in A-induced inner product:"
+      << std::endl;
+    std::cout << "(NOTE From left to right means from high- to low-frequency modes)" << std::endl;
+    std::cout << vector_to_string(geigenvalues) << std::endl;
+    std::cout << std::endl;
+
+    /// DEBUG
+    // FullMatrix<double> tmp(level_fullmatrix.m());
+    // FullMatrix<double> tmp2(level_fullmatrix.m());
+    // AE.mmult(tmp, Q);
+    // Q.Tmmult(tmp2, tmp);
+    // tmp2.print_formatted(std::cout);
+    // std::cout << std::endl;
+
+    const auto & dofh = subdomain_handler->get_dof_handler();
+
+    std::cout << "visualize generalized eigenvectors Q..." << std::endl;
+    {
+      std::vector<LinearAlgebra::distributed::Vector<double>> eigenvectors;
+      for(auto i = 0U; i < Q.n(); ++i)
       {
-        *pcout << "Rj A Rj^T:" << std::endl;
-        local_matrix.print_formatted(pcout->get_stream());
+        eigenvectors.emplace_back();
+        auto & v = eigenvectors.back();
+        level_matrix.initialize_dof_vector(v);
+        v = 0.;
+        AssertDimension(Q.m(), v.size());
+        AssertDimension(Q.n(), v.size());
+
+        for(auto j = 0U; j < Q.m(); ++j)
+          v(j) = Q(j, i);
+
+        visualize_dof_vector(
+          dofh, v, "eigvecs", 10, MappingQGeneric<dim>(1), "_" + Utilities::int_to_string(i, 4));
+      }
+    }
+
+    std::cout << "visualize A-mapped eigenvectors AQ..." << std::endl;
+    {
+      FullMatrix<double> AQ(level_fullmatrix.m());
+      level_fullmatrix.mmult(AQ, Q);
+      std::vector<LinearAlgebra::distributed::Vector<double>> eigenvectors;
+      for(auto i = 0U; i < Q.n(); ++i)
+      {
+        eigenvectors.emplace_back();
+        auto & v = eigenvectors.back();
+        level_matrix.initialize_dof_vector(v);
+        v = 0.;
+        AssertDimension(AQ.m(), v.size());
+        AssertDimension(AQ.n(), v.size());
+
+        for(auto j = 0U; j < AQ.m(); ++j)
+          v(j) = AQ(j, i);
+
+        visualize_dof_vector(
+          dofh, v, "Aeigvecs", 10, MappingQGeneric<dim>(1), "_" + Utilities::int_to_string(i, 4));
       }
     }
   }
