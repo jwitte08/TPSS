29013df7c597e5432bac6558f2e164c2ff77b7fc
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2020-12-14 17:07:47 +0100

    add c++17 flag

--- scripts/tpss-setup-ninja.sh
+++ scripts/tpss-setup-ninja.sh
@@ -1,5 +1,5 @@
 cmake -DCMAKE_BUILD_TYPE="Release" \
-      -DCMAKE_CXX_FLAGS="-march=native" \
+      -DCMAKE_CXX_FLAGS="-march=native -std=c++17" \
       -DCMAKE_CXX_FLAGS_RELEASE="-O3 -funroll-all-loops" \
       -GNinja \
       ..
