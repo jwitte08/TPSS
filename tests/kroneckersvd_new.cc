5f6987e40a19b9c99231138b2dbbafad5a4dbd53
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2021-01-11 13:30:36 +0100

    TEST compute_ksvd() for diagonal matrices

--- tests/kroneckersvd_new.cc
+++ tests/kroneckersvd_new.cc
@@ -99,6 +99,30 @@ check_kronecker_svd(const Table<2, Number> &                       matrix,
 
 
 
+template<typename Number>
+void
+check_kronecker_svd(const AlignedVector<Number> &                  diagonal,
+                    std::vector<std::array<Table<2, Number>, 2>> & ksvd,
+                    const std::size_t                              lanczos_iterations = -1)
+{
+  Tester tester;
+
+  compute_ksvd_new<Number>(diagonal, ksvd, lanczos_iterations);
+
+  DiagonalMatrixWrap<Number> matrix;
+  matrix.reinit(diagonal);
+
+  for(auto lane = 0U; lane < get_macro_size<Number>(); ++lane)
+  {
+    const auto fullmatrix = table_to_fullmatrix(matrix.as_table(), lane);
+    const auto fullksvd =
+      table_to_fullmatrix(Tensors::TensorProductMatrix<2, Number>(ksvd).as_table(), lane);
+    tester.compare_matrix(fullksvd, fullmatrix);
+  }
+}
+
+
+
 template<typename Number,
          bool do_zero_out_first_lane          = false,
          bool test_minimal_lanczos_iterations = false,
@@ -311,6 +335,123 @@ test_kronecker_svd_full()
 
 
 
+template<typename Number,
+         bool do_zero_out_first_lane          = false,
+         bool test_minimal_lanczos_iterations = false,
+         bool test_large_sizes                = false>
+void
+test_kronecker_svd_diagonal()
+{
+  const auto test_impl = [&](std::array<unsigned int, 2> rows,
+                             std::array<unsigned int, 2> columns) {
+    const auto zero_out_first_lane = [&](auto & rank1_tensors) {
+      for(auto & tensor : rank1_tensors)
+        for(auto & mat : tensor)
+          for(auto i = 0U; i < mat.size(0); ++i)
+            for(auto j = 0U; j < mat.size(1); ++j)
+              scalar_value(mat(i, j), /*lane*/ 0U) = 0.;
+    };
+
+    const auto check_kronecker_svd_impl = [&](auto &            rank1_tensors,
+                                              const std::size_t ksvd_rank_in = -1) {
+      if(do_zero_out_first_lane)
+        zero_out_first_lane(rank1_tensors);
+
+      const std::size_t ksvd_rank =
+        ksvd_rank_in == static_cast<std::size_t>(-1) ? rank1_tensors.size() : ksvd_rank_in;
+
+      std::vector<std::array<Table<2, Number>, 2>> ksvd =
+        Tensors::make_zero_rank1_tensors<2, Number>(ksvd_rank, rows, columns);
+
+      const auto get_diagonal =
+        [](const std::vector<std::array<Table<2, Number>, 2>> & rank1_tensors) {
+          Tensors::TensorProductMatrix<2, Number> tpm(rank1_tensors);
+          const auto &                            mat = tpm.as_table();
+          AssertDimension(mat.size(0), mat.size(1));
+          AlignedVector<Number> diagonal(mat.size(0));
+          for(auto i = 0U; i < mat.size(0); ++i)
+            diagonal[i] = mat(i, i);
+          return diagonal;
+        };
+
+      const std::size_t n_lanczos_iterations =
+        test_minimal_lanczos_iterations ? rank1_tensors.size() + 1 : -1;
+      check_kronecker_svd(get_diagonal(rank1_tensors), ksvd, n_lanczos_iterations);
+    };
+
+    const auto & [m1, m0] = rows;
+    const auto & [n1, n0] = columns;
+
+    const auto Id0 = Util::make_identity_matrix<Number>(m0, n0);
+    const auto Id1 = Util::make_identity_matrix<Number>(m1, n1);
+
+    const auto make_random_diagonal_matrix = [](const unsigned int m, const unsigned int n) {
+      Table<2, Number> mat = Util::make_random_matrix<Number>(m, n);
+      for(auto i = 0U; i < mat.size(0); ++i)
+        for(auto j = 0U; j < mat.size(1); ++j)
+          if(i != j)
+            mat(i, j) = static_cast<Number>(0.);
+      return mat;
+    };
+
+    const auto A0 = make_random_diagonal_matrix(m0, n0);
+    const auto A1 = make_random_diagonal_matrix(m1, n1);
+    const auto B0 = make_random_diagonal_matrix(m0, n0);
+    const auto B1 = make_random_diagonal_matrix(m1, n1);
+    const auto C0 = make_random_diagonal_matrix(m0, n0);
+    const auto C1 = make_random_diagonal_matrix(m1, n1);
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{Id1, Id0}};
+
+      check_kronecker_svd_impl(rank1_tensors, 1U);
+    }
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{Id1, Id0}, {Id1, Id0}};
+      check_kronecker_svd_impl(rank1_tensors, 1U);
+    }
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{Id1, Id0},
+                                                                 {Id1, Id0},
+                                                                 {Id1, Id0}};
+      check_kronecker_svd_impl(rank1_tensors, 1U);
+    }
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{A1, A0}, {A1, A0}, {A1, A0}};
+      check_kronecker_svd_impl(rank1_tensors, 1U);
+    }
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{B1, B0}, {A1, A0}};
+      check_kronecker_svd_impl(rank1_tensors);
+    }
+
+    {
+      std::vector<std::array<Table<2, Number>, 2>> rank1_tensors{{C1, C0}, {B1, B0}, {A1, A0}};
+      check_kronecker_svd_impl(rank1_tensors);
+    }
+  };
+
+  if(test_large_sizes)
+  {
+    test_impl({24U, 21U}, {24U, 21U});
+  }
+
+  else
+  {
+    test_impl({3U, 3U}, {3U, 3U});
+
+    test_impl({3U, 4U}, {3U, 4U});
+
+    test_impl({7U, 3U}, {7U, 3U});
+  }
+}
+
+
+
 void
 test_rank_two_kronecker_svd_full()
 {
@@ -374,8 +515,6 @@ test_rank_two_kronecker_svd_full()
   std::vector<std::array<Table<2, double>, 2>> ksvd_of_mat1 =
     Tensors::make_zero_rank1_tensors<2, double>(2U, rows, columns);
 
-  // const std::size_t lanczos_iterations = 2U;
-  // check_kronecker_svd<double>(mat1, ksvd_of_mat1, lanczos_iterations);
   check_kronecker_svd<double>(mat1, ksvd_of_mat1);
 }
 
@@ -443,8 +582,6 @@ test_rank_two_kronecker_svd()
   std::vector<std::array<Table<2, double>, 2>> ksvd_of_mat1 =
     Tensors::make_zero_rank1_tensors<2, double>(2U, rows, columns);
 
-  // const std::size_t lanczos_iterations = 2U;
-  // check_kronecker_svd<double>(mat1, ksvd_of_mat1, lanczos_iterations);
   check_kronecker_svd<double>(mat1, ksvd_of_mat1);
 }
 
@@ -489,8 +626,6 @@ test_rank_two_vectorized_kronecker_svd()
   std::vector<std::array<Table<2, VectorizedArray<double>>, 2>> ksvd_of_mat2 =
     Tensors::make_zero_rank1_tensors<2, VectorizedArray<double>>(2U, rows, columns);
 
-  // const std::size_t lanczos_iterations = 2U;
-  // check_kronecker_svd<VectorizedArray<double>>(mat2, ksvd_of_mat2, lanczos_iterations);
   check_kronecker_svd<VectorizedArray<double>>(mat2, ksvd_of_mat2);
 }
 
@@ -535,90 +670,83 @@ test_rank_one_kronecker_svd()
   std::vector<std::array<Table<2, double>, 2>> ksvd_of_mat1 =
     Tensors::make_zero_rank1_tensors<2, double>(1U, rows, columns);
 
-  // const std::size_t lanczos_iterations = 2U;
-  // check_kronecker_svd<double>(mat1, ksvd_of_mat1, lanczos_iterations);
   check_kronecker_svd<double>(mat1, ksvd_of_mat1);
-  // std::array<Table<2, double>, 2>              kp1    = {t1, t2};
-  // std::vector<std::array<Table<2, double>, 2>> mat1   = {kp1};
-  // std::vector<std::array<Table<2, double>, 2>> approx = {kp1};
-  // compute_ksvd_new<double>(mat1, approx);
-
-  // EXPECT_TRUE(Tensors::kronecker_product(approx[0][1], approx[0][0]) ==
-  //             Tensors::kronecker_product(t2, t1))
-  //   << "The rank one Kronecker SVD of the matrix does not match the matrix";
 }
 
 
 
-// void
-// test_diagonal_kronecker_svd()
-// {
-//   AlignedVector<double> diagonal(12);
-//   diagonal[0]  = 1;
-//   diagonal[1]  = 2;
-//   diagonal[2]  = 3;
-//   diagonal[3]  = 4;
-//   diagonal[4]  = 5;
-//   diagonal[5]  = 6;
-//   diagonal[6]  = 7;
-//   diagonal[7]  = 8;
-//   diagonal[8]  = 9;
-//   diagonal[9]  = 10;
-//   diagonal[10] = 11;
-//   diagonal[11] = 12;
-//   Table<2, double>                             t1(3, 3);
-//   Table<2, double>                             t2(4, 4);
-//   std::array<Table<2, double>, 2>              app_kp = {t1, t2};
-//   std::vector<std::array<Table<2, double>, 2>> approx = {app_kp, app_kp};
-//   compute_ksvd_new<double>(diagonal, approx);
-//   EXPECT_TRUE(std::abs(approx[0][1](0, 0) - 0.710868) <
-//               1e-6); // .710868 was generated by working python program, error would be smaller
-//               for
-//   // more digits
-//   EXPECT_TRUE(std::abs(approx[0][1](1, 1) - 1.735563) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][1](2, 2) - 2.760258) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][1](3, 3) - 3.784953) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](0, 0) - 2.545889) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](1, 1) - 2.899023) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](2, 2) - 3.252157) < 1e-6);
-// }
+void
+test_diagonal_kronecker_svd()
+{
+  AlignedVector<double> diagonal(12);
+  diagonal[0]  = 1;
+  diagonal[1]  = 2;
+  diagonal[2]  = 3;
+  diagonal[3]  = 4;
+  diagonal[4]  = 5;
+  diagonal[5]  = 6;
+  diagonal[6]  = 7;
+  diagonal[7]  = 8;
+  diagonal[8]  = 9;
+  diagonal[9]  = 10;
+  diagonal[10] = 11;
+  diagonal[11] = 12;
+
+  Table<2, double>                             t1(3, 3);
+  Table<2, double>                             t2(4, 4);
+  std::array<Table<2, double>, 2>              app_kp = {t1, t2};
+  std::vector<std::array<Table<2, double>, 2>> approx = {app_kp, app_kp};
+
+  compute_ksvd_new<double>(diagonal, approx);
+
+  // .710868 was generated by working python program, error would be smaller for more digits
+  EXPECT_TRUE(std::abs(approx[0][1](0, 0) - 0.710868) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](1, 1) - 1.735563) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](2, 2) - 2.760258) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](3, 3) - 3.784953) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](0, 0) - 2.545889) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](1, 1) - 2.899023) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](2, 2) - 3.252157) < 1e-6);
+}
 
 
 
-// void
-// test_diagonal_vectorized_kronecker_svd()
-// {
-//   AlignedVector<VectorizedArray<double>> diagonal(12);
-//   diagonal[0]    = make_vectorized_array<double>(1);
-//   diagonal[1]    = make_vectorized_array<double>(2);
-//   diagonal[2]    = make_vectorized_array<double>(3);
-//   diagonal[3]    = make_vectorized_array<double>(4);
-//   diagonal[3][1] = -12;
-//   diagonal[4]    = make_vectorized_array<double>(5);
-//   diagonal[5]    = make_vectorized_array<double>(6);
-//   diagonal[6]    = make_vectorized_array<double>(7);
-//   diagonal[7]    = make_vectorized_array<double>(8);
-//   diagonal[8]    = make_vectorized_array<double>(9);
-//   diagonal[8][1] = -33;
-//   diagonal[9]    = make_vectorized_array<double>(10);
-//   diagonal[10]   = make_vectorized_array<double>(11);
-//   diagonal[11]   = make_vectorized_array<double>(12);
-//   Table<2, VectorizedArray<double>>                             t1(3, 3);
-//   Table<2, VectorizedArray<double>>                             t2(4, 4);
-//   std::array<Table<2, VectorizedArray<double>>, 2>              app_kp = {t1, t2};
-//   std::vector<std::array<Table<2, VectorizedArray<double>>, 2>> approx = {app_kp, app_kp};
-//   compute_ksvd_new<VectorizedArray<double>>(diagonal, approx);
-//   EXPECT_TRUE(std::abs(approx[0][1](0, 0)[1] + 0.422376) <
-//               1e-6); // .710868 was generated by working python program, error would be smaller
-//               for
-//   // more digits
-//   EXPECT_TRUE(std::abs(approx[0][1](1, 1)[1] + 1.250524) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][1](2, 2)[1] - 5.699786) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][1](3, 3)[1] + 1.457304) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](0, 0)[1] - 1.097814) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](1, 1)[1] - 0.618112) < 1e-6);
-//   EXPECT_TRUE(std::abs(approx[0][0](2, 2)[1] + 5.896291) < 1e-6);
-// }
+void
+test_diagonal_vectorized_kronecker_svd()
+{
+  AlignedVector<VectorizedArray<double>> diagonal(12);
+  diagonal[0]    = make_vectorized_array<double>(1);
+  diagonal[1]    = make_vectorized_array<double>(2);
+  diagonal[2]    = make_vectorized_array<double>(3);
+  diagonal[3]    = make_vectorized_array<double>(4);
+  diagonal[3][1] = -12;
+  diagonal[4]    = make_vectorized_array<double>(5);
+  diagonal[5]    = make_vectorized_array<double>(6);
+  diagonal[6]    = make_vectorized_array<double>(7);
+  diagonal[7]    = make_vectorized_array<double>(8);
+  diagonal[8]    = make_vectorized_array<double>(9);
+  diagonal[8][1] = -33;
+  diagonal[9]    = make_vectorized_array<double>(10);
+  diagonal[10]   = make_vectorized_array<double>(11);
+  diagonal[11]   = make_vectorized_array<double>(12);
+
+  Table<2, VectorizedArray<double>>                             t1(3, 3);
+  Table<2, VectorizedArray<double>>                             t2(4, 4);
+  std::array<Table<2, VectorizedArray<double>>, 2>              app_kp = {t1, t2};
+  std::vector<std::array<Table<2, VectorizedArray<double>>, 2>> approx = {app_kp, app_kp};
+
+  compute_ksvd_new<VectorizedArray<double>>(diagonal, approx);
+
+  // .710868 was generated by working python program, error would be smaller for
+  // more digits
+  EXPECT_TRUE(std::abs(approx[0][1](0, 0)[1] + 0.422376) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](1, 1)[1] + 1.250524) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](2, 2)[1] - 5.699786) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][1](3, 3)[1] + 1.457304) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](0, 0)[1] - 1.097814) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](1, 1)[1] - 0.618112) < 1e-6);
+  EXPECT_TRUE(std::abs(approx[0][0](2, 2)[1] + 5.896291) < 1e-6);
+}
 
 
 
@@ -701,21 +829,42 @@ TEST(AnisotropicMinimalLanczosIterations_Double, compute_ksvd_full)
 
 
 
-// TEST(Diagonal_Double, compute_ksvd_diagonal)
-// {
-//   test_diagonal_kronecker_svd();
-// }
+TEST(HardcodedSimon_Double, compute_ksvd_diagonal)
+{
+  test_diagonal_kronecker_svd();
+}
 
-// TEST(Diagonal_VectorizedArrayDouble, compute_ksvd_diagonal)
-// {
-//   test_diagonal_vectorized_kronecker_svd();
-// }
+TEST(HardcodedSimon_VectorizedArrayDouble, compute_ksvd_diagonal)
+{
+  test_diagonal_vectorized_kronecker_svd();
+}
 
-TEST(RandomDiagonal_VectorizedArrayDouble, compute_ksvd_diagonal)
+TEST(AnisotropicSquared_Double, compute_ksvd_diagonal)
+{
+  test_kronecker_svd_diagonal<double>();
+}
+
+TEST(AnisotropicSquared_VectorizedArrayDouble, compute_ksvd_diagonal)
+{
+  test_kronecker_svd_diagonal<VectorizedArray<double>>();
+}
+
+TEST(AnisotropicSquaredMinimalLanczosIterations_Double, compute_ksvd_diagonal)
+{
+  test_kronecker_svd_diagonal<double, false, true>();
+}
+
+TEST(LargeAnisotropicSquared_Double, compute_ksvd_diagonal)
+{
+  test_kronecker_svd_diagonal<double, false, false, true>();
+}
+
+TEST(IsotropicSquared_VectorizedArrayDouble, compute_ksvd_diagonal)
 {
   using Number         = VectorizedArray<double>;
-  const unsigned int m = 10;
-  ConditionalOStream os(std::cout, false);
+  const unsigned int m = 3;
+
+  Tester tester;
 
   AlignedVector<Number> diagonal;
   diagonal.resize(m * m);
@@ -738,7 +887,7 @@ TEST(RandomDiagonal_VectorizedArrayDouble, compute_ksvd_diagonal)
   {
     const auto & kd_full   = table_to_fullmatrix(kd.as_table(), lane);
     const auto & reference = table_to_fullmatrix(diagonal_matrix, lane);
-    Util::compare_matrix(kd_full, reference, os);
+    tester.compare_matrix(kd_full, reference);
   }
 }
 
