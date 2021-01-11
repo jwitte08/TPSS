64bc43e8096bc1f5cb01e574a412c258e1ccb6d1
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2021-01-11 13:26:51 +0100

    compute_ksvd_new() - diagonal matrix path

--- include/solvers_and_preconditioners/TPSS/kroneckersvd_new.h
+++ include/solvers_and_preconditioners/TPSS/kroneckersvd_new.h
@@ -310,7 +310,7 @@ template<typename Number>
 void
 compute_ksvd_new(const Table<2, Number> &                       src,
                  std::vector<std::array<Table<2, Number>, 2>> & dst,
-                 std::size_t                                    lanczos_iterations_in = -1)
+                 const std::size_t                              lanczos_iterations_in = -1)
 {
   Assert(!dst.empty(), ExcMessage("dst is empty."));
   const auto & [B_0, A_0] = dst.front();
@@ -326,93 +326,33 @@ compute_ksvd_new(const Table<2, Number> &                       src,
 
 
 
-/*
-  Compute the low Kronecker rank approximation, i.e. the ksvd, of a diagonal matrix.
-  We compute the first few singular
-  values/vectors by using the Lanczos algorithm. The matricization of these
-  singular vectors then is the low Kronecker rank approximation. The matrix M is
-  passed in "in", and the low rank approximation is passed in "out"
-  to be consistent with dealII kroneckerproducts are passed as {B_i,A_i}
-*/
-
+/**
+ * Same as above except that a diagonal matrix @p src is passed instead of a
+ * tensor product matrix. This reduces the computational effort of
+ * matrix-vector-multiplatications with the shuffled matrix (or its
+ * transpose). Internally DiagonalMatrixWrap is used allowing for vectorized
+ * arithmetic type @p Number.
+ *
+ * Note that only squared diagonal matrices are supported so far.
+ */
 template<typename Number>
 void
-compute_ksvd_new(const AlignedVector<Number> &                  in,
-                 std::vector<std::array<Table<2, Number>, 2>> & out,
-                 std::size_t                                    lanczos_iterations = -1)
+compute_ksvd_new(const AlignedVector<Number> &                  diagonal,
+                 std::vector<std::array<Table<2, Number>, 2>> & dst,
+                 const std::size_t                              lanczos_iterations_in = -1)
 {
-  std::size_t tensor_rank_out = out.size();
-  if(lanczos_iterations == (std::size_t)-1)
-  {
-    lanczos_iterations = tensor_rank_out * tensor_rank_out + 10;
-  }
+  Assert(!dst.empty(), ExcMessage("dst is empty."));
+  const auto & [B_0, A_0] = dst.front();
+  AssertDimension(A_0.size(0) * B_0.size(0), diagonal.size());
+  AssertDimension(A_0.size(1) * B_0.size(1), diagonal.size());
 
-  std::size_t           big_m   = out[0][1].size(0);
-  std::size_t           small_m = out[0][0].size(0);
-  AlignedVector<Number> beta;
-  beta.push_back(Number(1)); // we artificially introduce a first value for
-  // beta to define beta.back()
-  AlignedVector<Number>              alpha;
-  std::vector<AlignedVector<Number>> r;
-  std::vector<AlignedVector<Number>> p = {AlignedVector<Number>(small_m * small_m)};
-  p.back()[0]                          = Number(1);
-  std::vector<AlignedVector<Number>> u = {AlignedVector<Number>(big_m * big_m)};
-  std::vector<AlignedVector<Number>> v;
-  for(std::size_t i = 0;
-      i < lanczos_iterations && std::abs(beta.back()) > std::numeric_limits<Number>::epsilon();
-      i++)
-  {
-    beta.push_back(std::sqrt(inner_product(p.back(), p.back())));
-    v.push_back(vector_inverse_scaling(p.back(), beta.back()));
-    r.push_back(vector_scaling(u.back(), -beta.back()));
-    r.back() = vector_addition(r.back(),
-                               reshuffled_diag_vector_multiplication(in, v.back(), big_m, small_m));
-    orthogonalize(r);
-    alpha.push_back(std::sqrt(inner_product(r.back(), r.back())));
-    u.push_back(vector_inverse_scaling(r.back(), alpha.back()));
-    p.push_back(vector_scaling(v.back(), -alpha.back()));
-    p.back() = vector_addition(
-      p.back(), transpose_reshuffled_diag_vector_multiplication(in, u.back(), big_m, small_m));
-    orthogonalize(p);
-  }
-  std::size_t           base_len = alpha.size() - 1;
-  Table<2, Number>      U(base_len, big_m * big_m); // discard first value of u since it is zero
-  Table<2, Number>      V(base_len,
-                     small_m * small_m); // discard last value of v since it is zero
-  AlignedVector<Number> real_beta(base_len -
-                                  1); // discard first two values of beta, first is artificially
-  // introduced, second only depends on inital guess
-  AlignedVector<Number> real_alpha(base_len); // discard last value of alpha since it is zero
-  for(std::size_t i = 0; i < base_len; i++)
-  {
-    real_alpha[i] = alpha[i];
-    if(i < base_len - 1)
-      real_beta[i] = beta[i + 2];
-    for(std::size_t j = 0; j < big_m * big_m; j++)
-      U(i, j) = u[i + 1][j];
-    for(std::size_t j = 0; j < small_m * small_m; j++)
-      V(i, j) = v[i][j];
-  }
+  DiagonalMatrixWrap<Number> shuffled_matrix;
+  shuffled_matrix.reinit(diagonal);
+  shuffled_matrix.shuffle({A_0.size(0), A_0.size(1)}, {B_0.size(0), B_0.size(1)});
 
-  AlignedVector<Number> singular_values(base_len);
-  Table<2, Number>      tildeU(base_len, base_len);
-  Table<2, Number>      tildeVT(base_len, base_len);
-
-  bidiagonal_svd(real_alpha, real_beta, tildeU, singular_values, tildeVT);
-
-  Table<2, Number> left_singular_vectors  = matrix_transpose_multiplication(tildeU, U);
-  Table<2, Number> right_singular_vectors = matrix_multiplication(tildeVT, V);
-
-  for(std::size_t i = 0; i < tensor_rank_out; i++)
-  {
-    for(std::size_t k = 0; k < big_m; k++)
-      for(std::size_t l = 0; l < big_m; l++)
-        out[i][1](k, l) = left_singular_vectors(i, k * big_m + l) * std::sqrt(singular_values[i]);
-    for(std::size_t k = 0; k < small_m; k++)
-      for(std::size_t l = 0; l < small_m; l++)
-        out[i][0](k, l) =
-          right_singular_vectors(i, k * small_m + l) * std::sqrt(singular_values[i]);
-  }
+  compute_ksvd_impl<Number, DiagonalMatrixWrap<Number>>(dst,
+                                                        shuffled_matrix,
+                                                        lanczos_iterations_in);
 }
 
 #endif
