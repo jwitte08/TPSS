e6fad0b645b760c014a8cc42058db0e47738d0ad
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2021-06-21 22:04:07 +0200

    WIP local C0IP solvers (Stokes)

--- apps/stokes_raviartthomasstream.cc
+++ apps/stokes_raviartthomasstream.cc
@@ -26,25 +26,35 @@ main(int argc, char * argv[])
     };
 
     //: default
-    unsigned int test_index                  = 5; // GMG_CG
-    unsigned int debug_depth                 = 0;
-    double       damping                     = 0.;
-    unsigned int force_mean_value_constraint = false;
-    double       ip_factor                   = 1.;
-    unsigned int n_cycles                    = 3;
-    unsigned int local_solver_variant        = 0;
-    unsigned int pde_index                   = 4; // NoSlip
-    int          n_threads_max               = 1;
+    unsigned int            test_index                  = 5; // GMG_CG
+    unsigned int            debug_depth                 = 0;
+    double                  damping                     = 0.;
+    unsigned int            force_mean_value_constraint = false;
+    double                  ip_factor                   = 1.;
+    unsigned int            n_cycles                    = 3;
+    unsigned int            local_solver_index          = 2; // C0IP
+    unsigned int            pde_index                   = 6; // NoSlipExp
+    int                     n_threads_max               = 1;
+    types::global_dof_index dof_limit_min               = 1e1;
+    types::global_dof_index dof_limit_max               = 1e5;
+    bool                    skip_A                      = false;
+    unsigned int            ksvd_rank                   = 1;
+    double                  local_stability_factor      = 1.;
 
     //: parse arguments
     atoi_if(test_index, 1);
     atoi_if(pde_index, 2);
     atoi_if(n_cycles, 3);
-    atoi_if(debug_depth, 4);
-    atof_if(damping, 5);
-    atoi_if(force_mean_value_constraint, 6);
-    atoi_if(local_solver_variant, 7);
-    atoi_if(n_threads_max, 8);
+    atoi_if(dof_limit_min, 4);
+    atoi_if(dof_limit_max, 5);
+    atoi_if(local_solver_index, 6);
+    atoi_if(ksvd_rank, 7);
+    atof_if(local_stability_factor, 8);
+    atoi_if(debug_depth, 9);
+    atof_if(damping, 10);
+    atoi_if(force_mean_value_constraint, 11);
+    atoi_if(skip_A, 12);
+    atoi_if(n_threads_max, 13);
 
     deallog.depth_console(debug_depth);
     Utilities::MPI::MPI_InitFinalize mpi_initialization(argc,
@@ -70,8 +80,9 @@ main(int argc, char * argv[])
         damping = TPSS::lookup_damping_factor(patch_variant, smoother_variant, dim);
     }
 
-    options.setup(test_index, damping);
-    options.prms.n_cycles = n_cycles;
+    options.setup(test_index, damping / local_stability_factor);
+    options.prms.n_cycles   = n_cycles;
+    options.prms.dof_limits = {dof_limit_min, dof_limit_max};
     options.prms.multigrid.pre_smoother.schwarz.n_active_blocks =
       options.prms.multigrid.post_smoother.schwarz.n_active_blocks = 2;
 
@@ -79,22 +90,43 @@ main(int argc, char * argv[])
     AssertThrow(pde_index < EquationData::n_variants,
                 ExcMessage("This equation is not implemented."));
     equation_data.variant = static_cast<EquationData::Variant>(pde_index);
-    // if(options.prms.solver.variant == "GMRES_GMG" || options.prms.solver.variant == "CG_GMG")
-    //   equation_data.local_kernel_size = 1U;
     AssertThrow(force_mean_value_constraint == 0 || force_mean_value_constraint == 1,
                 ExcMessage("Invalid."));
+
     equation_data.do_mean_value_constraint = force_mean_value_constraint;
     if(options.prms.solver.variant == "direct")
       equation_data.do_mean_value_constraint = true;
-    equation_data.ip_factor    = ip_factor;
-    equation_data.local_solver = static_cast<LocalSolver>(local_solver_variant);
+
+    equation_data.ip_factor = ip_factor;
+
+    equation_data.local_solver        = static_cast<LocalSolver>(local_solver_index);
+    equation_data.ksvd_tensor_indices = [&]() -> std::set<unsigned int> {
+      if(ksvd_rank == 1U)
+        return {0U};
+      else if(ksvd_rank == 2U)
+        return {0U, 1U};
+      else if(ksvd_rank == 12U)
+        return {0U, 2U};
+      else if(ksvd_rank == 3U)
+        return {0U, 1U, 2U};
+      else
+        AssertThrow(false, ExcMessage("KSVD rank isn't supported."));
+      return {};
+    }();
+    equation_data.n_lanczos_iterations = 4 + 1;
+    if(equation_data.ksvd_tensor_indices == std::set<unsigned int>{0U, 1U})
+      equation_data.addition_to_min_eigenvalue = 0.025;
+
+    equation_data.skip_A = skip_A;
+
+    const auto filename = get_filename(options.prms, equation_data);
 
     std::fstream fout;
-    const auto   filename = get_filename(options.prms, equation_data);
+    fout.open(filename + ".log", std::ios_base::out);
 
-    const auto pcout = std::make_shared<ConditionalOStream>(std::cout, is_first_proc);
+    const auto pcout = std::make_shared<ConditionalOStream>(fout, is_first_proc);
 
-    using StokesProblem = ModelProblem<dim, fe_degree_p, Method::RaviartThomasStream>;
+    using StokesProblem = ModelProblem<dim, fe_degree_p, Method::RaviartThomasStream, false>;
     StokesProblem stokes_problem(options.prms, equation_data);
     stokes_problem.pcout = pcout;
 
@@ -105,6 +137,8 @@ main(int argc, char * argv[])
       write_ppdata_to_string(stokes_problem.pp_data, stokes_problem.pp_data_pressure);
 
     *pcout << std::endl << std::endl << results_as_string;
+
+    fout.close();
 
     if(is_first_proc)
     {
