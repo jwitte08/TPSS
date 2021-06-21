71d8227051e96b32b74d124373300d5c8d157fb8
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2021-06-21 20:29:39 +0200

    WIP local stability (not-simplified Stokes)
    + determine w_opt for inexact AVS

--- apps/stokes_optdamping.cc
+++ apps/stokes_optdamping.cc
@@ -52,6 +52,7 @@ main(int argc, char * argv[])
   bool         do_mirror_p_v_block = true;
   unsigned int max_size            = 60;
   bool         skip_A              = false;
+  unsigned int ksvd_rank           = 1;
   ///
   double       ip_factor     = 1.;
   int          n_threads_max = 1;
@@ -65,9 +66,11 @@ main(int argc, char * argv[])
   atof_if(omega, 4);
   atof_if(local_solver_index, 5);
   atoi_if(do_mirror_p_v_block, 6);
-  atoi_if(max_size, 7);
-  atoi_if(skip_A, 8);
+  atoi_if(ksvd_rank, 7);
+  atoi_if(max_size, 8);
+  atoi_if(skip_A, 9);
 
+  damping /= omega;
   AssertThrow(damping <= 1., ExcMessage("Over-relaxation is not allowed."));
 
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc,
@@ -79,37 +82,92 @@ main(int argc, char * argv[])
   const bool is_first_proc = Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0U;
   const auto n_mpi_procs   = Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);
 
-  AssertThrow(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1U,
-              ExcMessage("MPI not supported."));
+  AssertThrow(n_mpi_procs == 1U, ExcMessage("MPI not supported."));
 
-  StokesFlow     options;
-  constexpr auto dim          = CT::DIMENSION_;
-  constexpr auto fe_degree_sf = CT::FE_DEGREE_;
-  constexpr auto fe_degree_p  = fe_degree_sf - 1;
-  // constexpr auto patch_variant    = CT::PATCH_VARIANT_;
-  // constexpr auto smoother_variant = CT::SMOOTHER_VARIANT_;
-  constexpr bool   is_simplified   = true;
-  constexpr double threshold_noise = 1.e-10;
+  StokesFlow       options;
+  constexpr auto   dim              = CT::DIMENSION_;
+  constexpr auto   fe_degree_sf     = CT::FE_DEGREE_;
+  constexpr auto   fe_degree_p      = fe_degree_sf - 1;
+  constexpr auto   patch_variant    = CT::PATCH_VARIANT_;
+  constexpr auto   smoother_variant = CT::SMOOTHER_VARIANT_;
+  constexpr bool   is_simplified    = false; // !!!
+  constexpr double threshold_noise  = 1.e-10;
+
+  const auto & make_filename = [&](const RT::Parameter & prms,
+                                   const EquationData &  equation_data) -> std::string {
+    std::ostringstream oss;
+
+    const auto &      pre_schwarz = prms.multigrid.pre_smoother.schwarz;
+    const std::string str_schwarz_variant =
+      TPSS::getstr_schwarz_variant(pre_schwarz.patch_variant, pre_schwarz.smoother_variant);
+    const unsigned int n_cells_1d = (1 << prms.mesh.n_refinements) * prms.mesh.n_repetitions;
+    const double       damping    = pre_schwarz.damping_factor;
+
+    oss << (is_simplified ? "simplestokes_optdamping" : "stokes_optdamping");
+    oss << std::scientific << std::setprecision(2);
+    oss << "_" << CT::DIMENSION_ << "D";
+    oss << "_" << CT::FE_DEGREE_ << "deg";
+    AssertDimension(CT::DIMENSION_, 2);
+    oss << "_" << n_cells_1d << "x" << n_cells_1d << "mesh";
+    oss << "_" << Util::damping_to_fstring(damping) << "damp";
+    if(prms.multigrid.pre_smoother.variant == SmootherParameter::SmootherVariant::Schwarz)
+    {
+      oss << "_" << str_schwarz_variant;
+      oss << "_" << equation_data.sstr_local_solver();
+      if(equation_data.local_solver == LocalSolver::C0IP_KSVD)
+        for(const auto index : equation_data.ksvd_tensor_indices)
+          oss << index;
+    }
+
+    return oss.str();
+  };
 
   options.setup(test_index, damping);
   options.prms.multigrid.pre_smoother.schwarz.n_active_blocks =
     options.prms.multigrid.post_smoother.schwarz.n_active_blocks = 2;
-  options.prms.mesh.n_refinements                                = n_refinements;
-  options.prms.mesh.n_repetitions                                = n_repetitions;
+  options.prms.multigrid.pre_smoother.n_smoothing_steps =
+    options.prms.multigrid.post_smoother.n_smoothing_steps = 1;
+  options.prms.multigrid.pre_smoother.schwarz.symmetrize_smoothing =
+    options.prms.multigrid.post_smoother.schwarz.symmetrize_smoothing =
+      (smoother_variant == TPSS::SmootherVariant::multiplicative ? true : false);
+  options.prms.mesh.n_refinements = n_refinements;
+  options.prms.mesh.n_repetitions = n_repetitions;
 
   EquationData equation_data;
   AssertThrow(pde_index < EquationData::n_variants,
               ExcMessage("This equation is not implemented."));
-  equation_data.variant           = static_cast<EquationData::Variant>(pde_index);
-  equation_data.ip_factor         = ip_factor;
-  equation_data.local_solver      = static_cast<LocalSolver>(local_solver_index);
-  equation_data.local_kernel_size = 1U;
-  equation_data.skip_A            = skip_A;
+  equation_data.variant             = static_cast<EquationData::Variant>(pde_index);
+  equation_data.ip_factor           = ip_factor;
+  equation_data.local_solver        = static_cast<LocalSolver>(local_solver_index);
+  equation_data.local_kernel_size   = equation_data.local_solver > LocalSolver::C0IP ? 0U : 1U;
+  equation_data.ksvd_tensor_indices = [&]() -> std::set<unsigned int> {
+    if(ksvd_rank == 1U)
+      return {0U};
+    else if(ksvd_rank == 2U)
+      return {0U, 1U};
+    else if(ksvd_rank == 12U)
+      return {0U, 2U};
+    else if(ksvd_rank == 3U)
+      return {0U, 1U, 2U};
+    else
+      AssertThrow(false, ExcMessage("KSVD rank isn't supported."));
+    return {};
+  }();
+  equation_data.n_lanczos_iterations = 4 + 1;
+  if(equation_data.ksvd_tensor_indices == std::set<unsigned int>{0U, 1U})
+    equation_data.addition_to_min_eigenvalue = 0.025;
+  equation_data.skip_A = skip_A;
 
-  const auto pcout = std::make_shared<ConditionalOStream>(std::cout, is_first_proc);
+  const auto filename = make_filename(options.prms, equation_data);
 
+  std::fstream fout;
+  fout.open(filename + ".log", std::ios_base::out);
+
+  const auto pcout = std::make_shared<ConditionalOStream>(fout, is_first_proc);
+
+  // !!!
   using StokesProblem = ModelProblem<dim, fe_degree_p, Method::RaviartThomasStream, is_simplified>;
-  // using StokesProblem = ModelProblem<dim, fe_degree_p, Method::RaviartThomas, is_simplified>; //
+  // using StokesProblem = ModelProblem<dim, fe_degree_p, Method::RaviartThomas, is_simplified>;
   // !!!
   StokesProblem stokes_problem(options.prms, equation_data);
   stokes_problem.pcout = pcout;
@@ -122,27 +180,12 @@ main(int argc, char * argv[])
 
   AssertThrow(mgc->mg_schwarz_smoother_pre, ExcMessage("Check runtime parameters."));
 
-  // using MatrixIntegrator =
-  //   VelocityPressure::FD::MatrixIntegratorStream<dim, fe_degree_p, double, is_simplified>;
-  // using local_matrix_type = typename MatrixIntegrator::matrix_type;
-
   const auto                                  level          = mgc->mg_matrices.max_level();
   const TrilinosWrappers::BlockSparseMatrix & level_matrix   = mgc->mg_matrices[level];
   const types::global_dof_index               n_level_dofs_v = level_matrix.block(0, 0).m();
 
-  const auto   mgss              = mgc->mg_schwarz_smoother_pre;
-  const auto   subdomain_handler = mgss->get_subdomain_handler(level);
-  const auto & partition_data    = subdomain_handler->get_partition_data();
-
-  // const auto                     n_subdomains = partition_data.n_subdomains();
-  // std::vector<local_matrix_type> local_matrices(n_subdomains);
-
-  // MatrixIntegrator integrator;
-  // integrator.initialize(equation_data);
-  // integrator.assemble_subspace_inverses(*subdomain_handler,
-  //                                       local_matrices,
-  //                                       level_matrix,
-  //                                       partition_data.get_patch_range());
+  const auto mgss              = mgc->mg_schwarz_smoother_pre;
+  const auto subdomain_handler = mgss->get_subdomain_handler(level);
 
   auto         patch_transfer     = mgc->mg_matrices[level].get_patch_transfer(*subdomain_handler);
   const auto & patch_dof_worker_v = patch_transfer->get_patch_dof_worker(0);
@@ -156,9 +199,9 @@ main(int argc, char * argv[])
     if(matrix.n() > max_size)
       return;
     remove_noise_from_matrix(matrix, threshold_noise);
-    std::cout << description << std::endl;
-    matrix.print_formatted(std::cout);
-    std::cout << std::endl;
+    fout << description << std::endl;
+    matrix.print_formatted(fout);
+    fout << std::endl;
   };
 
   const auto extract_interior_dofs = [&](const unsigned int block_index) {
@@ -233,7 +276,7 @@ main(int argc, char * argv[])
       const auto n_dofs_v = dof_indices_on_patch.front().size();
 
       auto tildeAj_inv = table_to_fullmatrix(Tensors::inverse_matrix_to_table(local_solver), lane);
-      // tildeAj_inv /= omega;
+      tildeAj_inv /= omega;
       if(do_mirror_p_v_block)
         mirror_p_v_block(tildeAj_inv, n_dofs_v);
       print_fullmatrix(tildeAj_inv, "tildeAj_inv");
@@ -242,8 +285,8 @@ main(int argc, char * argv[])
       // auto Cj = table_to_fullmatrix(Tensors::matrix_to_table(local_solver.solver_sf), lane);
       // print_fullmatrix(Cj, "Cj (stream function problem):");
       // const auto eigenvalues_Cj = compute_eigenvalues(Cj);
-      // std::cout << "complex eigenvalues of Cj (stream function problem): " << std::endl;
-      // std::cout << vector_to_string(eigenvalues_Cj) << std::endl;
+      // fout << "complex eigenvalues of Cj (stream function problem): " << std::endl;
+      // fout << vector_to_string(eigenvalues_Cj) << std::endl;
 
       FullMatrix<double> Aj(patch_transfer->n_dofs_per_patch());
       for(auto bj = 0U; bj < 2U; ++bj)
@@ -268,13 +311,13 @@ main(int argc, char * argv[])
       print_fullmatrix(approx_identity, "tildeAj^{-1} Rj A Rj^T:");
 
       const auto & evs = compute_eigenvalues(approx_identity);
-      std::cout << "eigenvalues of tildeAj^{-1} Rj A Rj^T " << std::endl;
-      std::cout << vector_to_string(evs) << std::endl;
+      fout << "eigenvalues of tildeAj^{-1} Rj A Rj^T " << std::endl;
+      fout << vector_to_string(evs) << std::endl;
 
       // FullMatrix<double> Q(Aj.m());
       // const auto &       geigenvalues = compute_generalized_eigenvalues_symm(Aj, tildeAj_inv, Q);
-      // std::cout << "generalized eigenvalues of Rj A Rj^T x = lambda Aj x:" << std::endl;
-      // std::cout << vector_to_string(geigenvalues) << std::endl;
+      // fout << "generalized eigenvalues of Rj A Rj^T x = lambda Aj x:" << std::endl;
+      // fout << vector_to_string(geigenvalues) << std::endl;
     }
   }
 
@@ -308,79 +351,8 @@ main(int argc, char * argv[])
 
   {
     const auto complex_eigenvalues = compute_eigenvalues(E);
-    std::cout << "complex eigenvalues of E:" << std::endl;
-    std::cout << vector_to_string(complex_eigenvalues) << std::endl;
-
-    // FullMatrix<double> AE(level_fullmatrix.m());
-    // level_fullmatrix.mmult(AE, E);
-    // FullMatrix<double> Q(level_fullmatrix.m());
-    // const auto &       geigenvalues = compute_generalized_eigenvalues_symm(AE, level_fullmatrix,
-    // Q); std::cout
-    //   << "Generalized eigenvalues for AEx = Ax:\n"
-    //   << "NOTE These are the eigenvalues of error propagation E in the A-induced inner
-    //   product.\n"
-    //   << "NOTE From left to right means from high to low frequency modes.\n";
-    // std::cout << vector_to_string(geigenvalues) << std::endl;
-    // std::cout << std::endl;
-
-    //   /// DEBUG
-    //   // FullMatrix<double> tmp(level_fullmatrix.m());
-    //   // FullMatrix<double> tmp2(level_fullmatrix.m());
-    //   // AE.mmult(tmp, Q);
-    //   // Q.Tmmult(tmp2, tmp);
-    //   // tmp2.print_formatted(std::cout);
-    //   // std::cout << std::endl;
-
-    //   if(Q.n() <= max_size && dim == 2)
-    //   {
-    //     std::cout << "visualize generalized eigenvectors Q..." << std::endl;
-    //     {
-    //       std::vector<LinearAlgebra::distributed::Vector<double>> eigenvectors;
-    //       for(auto i = 0U; i < Q.n(); ++i)
-    //       {
-    //         eigenvectors.emplace_back();
-    //         auto & v = eigenvectors.back();
-    //         level_matrix.initialize_dof_vector(v);
-    //         v = 0.;
-
-    //         if(interior_dofs.empty())
-    //           for(auto j = 0U; j < Q.m(); ++j)
-    //             v(j) = Q(j, i);
-    //         else
-    //           for(auto j = 0U; j < Q.m(); ++j)
-    //             v(interior_dofs[j]) = Q(j, i);
-
-    //         visualize_dof_vector(
-    //           dofh, v, "eigvecs", 10, MappingQGeneric<dim>(1), "_" + Utilities::int_to_string(i,
-    //           4));
-    //       }
-    //     }
-
-    //     std::cout << "visualize A-mapped eigenvectors AQ..." << std::endl;
-    //     {
-    //       FullMatrix<double> AQ(level_fullmatrix.m());
-    //       level_fullmatrix.mmult(AQ, Q);
-    //       std::vector<LinearAlgebra::distributed::Vector<double>> eigenvectors;
-    //       for(auto i = 0U; i < Q.n(); ++i)
-    //       {
-    //         eigenvectors.emplace_back();
-    //         auto & v = eigenvectors.back();
-    //         level_matrix.initialize_dof_vector(v);
-    //         v = 0.;
-
-    //         if(interior_dofs.empty())
-    //           for(auto j = 0U; j < AQ.m(); ++j)
-    //             v(j) = AQ(j, i);
-    //         else
-    //           for(auto j = 0U; j < AQ.m(); ++j)
-    //             v(interior_dofs[j]) = AQ(j, i);
-
-    //         visualize_dof_vector(
-    //           dofh, v, "Aeigvecs", 10, MappingQGeneric<dim>(1), "_" + Utilities::int_to_string(i,
-    //           4));
-    //       }
-    //     }
-    //   }
+    fout << "complex eigenvalues of E:" << std::endl;
+    fout << vector_to_string(complex_eigenvalues) << std::endl;
   }
 
   return 0;
